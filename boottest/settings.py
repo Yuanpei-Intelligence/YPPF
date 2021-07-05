7a191eac8d09ecc9ebd6cbc3f7edb772356e4a2b
YWolfeee <1800017704@pku.edu.cn>
2021-07-05 11:24:43 +0800

    YWolfeee: delete unnecessary file

--- boottest/settings.py
+++ boottest/settings.py
@@ -93,8 +93,8 @@ DATABASES = {
         'NAME': 'YPPF',
         'HOST': '127.0.0.1',
         'PORT': 3306,
-        'USER': 'root',
-        'PASSWORD': 'root',
+        'USER': '',
+        'PASSWORD': '',
         # 'OPTIONS': {
         #     "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
         # }
