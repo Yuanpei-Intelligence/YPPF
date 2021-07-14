55c859a67a4ef5630ac2aa5934b312dfd48c1bb3
qingzhenduyu <2280936809@qq.com>
2021-07-14 16:28:54 +0800

    替换变量名

--- app/admin.py
+++ app/admin.py
@@ -2,14 +2,14 @@ from django.contrib import admin
 from app.models import *
 # Register your models here.
 class Newstudent(admin.ModelAdmin):
-    list_display = ['sno','sname','username','sgender','syear','semail','sclass','smajor','stel','firstTimeLogin']
-    search_fields = ('sname','sno')
+    list_display = ['pno','pname','username','pgender','pyear','pemail','pclass','pmajor','ptel','firstTimeLogin']
+    search_fields = ('pname','pno')
 class NewOrg(admin.ModelAdmin):
     list_display = ['organization_name','department']
     search_fields = ('organization_name','department')
 class NewPos(admin.ModelAdmin):
     list_display = ['position_stu','from_organization','job']
-    search_fields = ('position_stu__sname','from_organization__organization_name','position_stu__sno')
-admin.site.register(naturalPeople,Newstudent)
+    search_fields = ('position_stu__sname','from_organization__organization_name','position_stu__pno')
+admin.site.register(NaturalPeople,Newstudent)
 admin.site.register(organization, NewOrg)
 admin.site.register(position, NewPos)
--- app/models.py
+++ app/models.py
@@ -4,70 +4,86 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 # Create your models here.
 import datetime
-class naturalPeopleManager(models.Manager):
+
+
+class NaturalPeopleManager(models.Manager):
     def activated(self):
-        return self.exclude(sstatus=naturalPeople.status.GRADUATED)
-    def set_status(self):#修改毕业状态，每年调用一次
-        datas=naturalPeople.objects.activated()
-        year=datetime.datetime.now().strftime('%Y')
-        datas.objects.filter(syear=str(int(year)-4)).update(sstatus=1)
+        return self.exclude(sstatus=NaturalPeople.status.GRADUATED)
+
+    def set_status(self):  # 修改毕业状态，每年调用一次
+        datas = NaturalPeople.objects.activated()
+        year = datetime.datetime.now().strftime('%Y')
+        datas.objects.filter(pyear=str(int(year) - 4)).update(sstatus=1)
+
+
 # Create your models here.
-class naturalPeople(models.Model):
-    sno = models.CharField(max_length=10, unique=True, primary_key=True)
-    sname = models.CharField(max_length=10, null=True)
+class NaturalPeople(models.Model):
+    pno = models.CharField(max_length=10, unique=True, primary_key=True)
+    pname = models.CharField(max_length=10, null=True)
     username = models.ForeignKey(User, to_field='username', on_delete=models.CASCADE)
-    sgender = models.CharField(max_length=10, null=True)
-    syear = models.CharField(max_length=5, null=True)
+    pgender = models.CharField(max_length=10, null=True)
+    pyear = models.CharField(max_length=5, null=True)
 
-    sdorm =models.CharField(max_length=6,null=True)#宿舍
-    class status (models.IntegerChoices):
-        UNDERGRADUATED=0;#未毕业
-        GRADUATED=1;#毕业则注销
-    sstatus=models.SmallIntegerField('在校状态',choices=status.choices,default=0)#账户状态
+    pdorm = models.CharField(max_length=6, null=True)  # 宿舍
+
+    class status(models.IntegerChoices):
+        UNDERGRADUATED = 0;  # 未毕业
+        GRADUATED = 1;  # 毕业则注销
+
+    sstatus = models.SmallIntegerField('在校状态', choices=status.choices, default=0)  # 账户状态
+
     class Identity(models.IntegerChoices):
-        TEACHER=0;
-        STUDENT=1;
-    TypeID=models.SmallIntegerField('身份',choices=Identity.choices,default=0)#标识学生还是老师
+        TEACHER = 0;
+        STUDENT = 1;
 
-    semail = models.CharField(max_length=100,null=True)
-    sclass = models.CharField(max_length=5,null=True)
-    smajor = models.CharField(max_length=25,null=True)
-    stel = models.CharField(max_length=20,null=True)
-    sBio = models.TextField(max_length=1024,default='还没有填写哦～')
-    avatar = models.ImageField(upload_to=f'avatar/',blank=True)
+    TypeID = models.SmallIntegerField('身份', choices=Identity.choices, default=1)  # 标识学生还是老师
+
+    pemail = models.CharField(max_length=100, null=True)
+    pclass = models.CharField(max_length=5, null=True)
+    pmajor = models.CharField(max_length=25, null=True)
+    ptel = models.CharField(max_length=20, null=True)
+    pBio = models.TextField(max_length=1024, default='还没有填写哦～')
+    avatar = models.ImageField(upload_to=f'avatar/', blank=True)
     firstTimeLogin = models.BooleanField(default=True)
-    objects=naturalPeopleManager()
+    objects = NaturalPeopleManager()
+
     def __str__(self):
-        return "学生：" + str(self.syear) + '级 ' + str(self.sno) + ' ' + str(self.sname)
-    def hand_set_status(self):#延毕情况后续实现
+        return "学生：" + str(self.pyear) + '级 ' + str(self.pno) + ' ' + str(self.pname)
+
+    def hand_set_status(self):  # 延毕情况后续实现
         pass
+
+
 class organization(models.Model):
-    organization_name = models.CharField(max_length=32,default='暂未加入学工组织')
-    department = models.CharField(max_length=32,null=True,blank=True)
+    organization_name = models.CharField(max_length=32, default='暂未加入学工组织')
+    department = models.CharField(max_length=32, null=True, blank=True)
+
     def __str__(self):
         if self.department is not None:
             return self.organization_name + str(self.department)
         else:
             return self.organization_name
 
+
 class position(models.Model):
     position_stu = models.ForeignKey(
-        naturalPeople, related_name='position_stu', on_delete=models.CASCADE,
+        NaturalPeople, related_name='position_stu', on_delete=models.CASCADE,
     )
     from_organization = models.ForeignKey(
-        organization, related_name='org_from',on_delete=models.CASCADE)#,default=organization.objects.get(organization_name='暂未加入学工组织'))
+        organization, related_name='org_from',
+        on_delete=models.CASCADE)  # ,default=organization.objects.get(organization_name='暂未加入学工组织'))
 
-    job = models.CharField(verbose_name='职务',max_length=32,default='无')
+    job = models.CharField(verbose_name='职务', max_length=32, default='无')
 
     def __str__(self):
         return '机构：' + str(self.from_organization.organization_name) + '; 部门：' + str(self.from_organization.department)
-        + "；负责人：" + str(self.position_stu.sname)
+        + "；负责人：" + str(self.position_stu.pname)
 
-#@receiver(post_save,sender=student)
-#def create_position(sender, instance, created, **kwargs):
+# @receiver(post_save,sender=student)
+# def create_position(sender, instance, created, **kwargs):
 #    if created:
 #        position.objects.create(position_stu=instance)
-        
+
 # @receiver(post_save, sender=student)
 # def save_position(sender, instance, **kwargs):
 #     instance.s
