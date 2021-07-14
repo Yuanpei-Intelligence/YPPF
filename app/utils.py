eff6eab5eba500fafcc5bcc4cf57f11f82d75d06
ROC-sudo <1800017716@pku.edu.cn>
2021-07-14 17:12:02 +0800

    Use @required_login as far as possible
    Change authentication method ( md5 to sha256, add time stamp ) for underground app.
    
    by pkuGenuine July.14

--- app/utils.py
+++ app/utils.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.hashers import BasePasswordHasher,MD5PasswordHasher ,mask_hash  
-import json
 import hashlib
+import json
 class MyMD5PasswordHasher(MD5PasswordHasher):  
     algorithm = "mymd5"
     salt = "" 
@@ -13,12 +13,25 @@ class MyMD5PasswordHasher(MD5PasswordHasher):
         hash = hashlib.md5(password).hexdigest().upper()  
         return hash  
   
-    def verify(self, password, encoded):  
+    def verify(self, password, encoded):
         encoded_2 = self.encode(password)  
         return encoded.upper() == encoded_2.upper()  
 
+class MySHA256Hasher(object):
+    def __init__(self, secret):
+        self.secret = secret
+
+    def encode(self, identifier):
+        assert identifier is not None
+        identifier = (identifier + self.secret).encode('utf-8')
+        return hashlib.sha256(identifier).hexdigest().upper()
+
+    def verify(self, identifier, encoded):
+        encoded_2 = self.encode(identifier)
+        return encoded.upper() == encoded_2.upper()
+
 def load_local_json(path='./local_json.json'):
     local_dict = {}
-    with open(path, encoding='utf_8') as f:
+    with open(path) as f:
         local_dict = json.load(f)
     return local_dict
