0f3aa70962e01ee584c45e9ea4411e2bd529a9d6
YWolfeee <48834325+YWolfeee@users.noreply.github.com>
2021-07-19 23:55:58 +0800

    YWolfeee:采用统一的html储存user左边栏,用include html

--- boottest/__init__.py
+++ boottest/__init__.py
@@ -1,3 +1,12 @@
 import pymysql
+import json
 pymysql.install_as_MySQLdb()
 pymysql.version_info = (1, 4, 6, 'final', 0)
+
+def load_local_json(path='./local_json.json'):
+    local_dict = {}
+    with open(path, encoding='utf_8') as f:
+        local_dict = json.load(f)
+    return local_dict
+
+local_dict = load_local_json()
