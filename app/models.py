e02941ea4413499937bbde53669da901e7eda27d
sunyb-dot <18211195741@126.com>
2021-07-21 16:15:14 +0800

    changes in models.py

--- app/models.py
+++ app/models.py
@@ -30,7 +30,8 @@ class NaturalPerson(models.Model):
     #                        on_delete=models.CASCADE, unique=True, primary_key=True)
     pid = models.OneToOneField(to=User, on_delete=models.CASCADE)
     pname = models.CharField("姓名", max_length=10)
-    pnickname = models.CharField("昵称", max_length=20, null=True, blank=True)  # 添加昵称
+    pnickname = models.CharField(
+        "昵称", max_length=20, null=True, blank=True)  # 添加昵称
 
     class Gender(models.IntegerChoices):
         MALE = (0, "男")
@@ -38,7 +39,8 @@ class NaturalPerson(models.Model):
         OTHER = (2, "其它")
 
     # pgender = models.CharField(max_length=10, null=True)
-    pgender = models.SmallIntegerField('性别', choices=Gender.choices, null=True, blank=True)
+    pgender = models.SmallIntegerField(
+        '性别', choices=Gender.choices, null=True, blank=True)
 
     pemail = models.EmailField("邮箱", null=True, blank=True)
     ptel = models.CharField("电话", max_length=20, null=True, blank=True)
@@ -81,6 +83,25 @@ class NaturalPerson(models.Model):
 
     def __str__(self):
         return str(self.pname)
+
+    def show_info(self):
+        '''
+            返回值为一个列表，在search.html中使用，按照如下顺序呈现：
+            people_field = ['姓名', '年级&班级', '昵称', '性别', '专业', '邮箱', '电话', '宿舍', '状态']
+            其中未公开的属性呈现为‘未公开’
+        '''
+        # print(self.pyear)
+        unpublished = '未公开'
+        # print(unpublished)
+        info = [self.pname, self.pyear, self.pclass]
+        info.append(self.pnickname if self.show_nickname else unpublished)
+        info.append(self.pgender if self.show_gender else unpublished)
+        info.append(self.pmajor if self.show_major else unpublished)
+        info.append(self.pemail if self.show_email else unpublished)
+        info.append(self.ptel if self.show_tel else unpublished)
+        info.append(self.pdorm if self.show_dorm else unpublished)
+        info.append(self.pstatus)
+        return info
 
 
 class OrganizationType(models.Model):
@@ -155,12 +176,14 @@ class Organization(models.Model):
     # oschool_semester = models.CharField(
     #    "当前学期", choices=Semester.choices,max_length=15)
 
-    ostatus = models.BooleanField("激活状态", default=False)  # 表示一个组织是否上线(或者是已经被下线)
+    ostatus = models.BooleanField(
+        "激活状态", default=False)  # 表示一个组织是否上线(或者是已经被下线)
 
     objects = OrganizationManager()
 
     YQPoint = models.FloatField("元气值", default=0.0)
-    ointroduction = models.TextField('介绍', null=True, blank=True, default="这里暂时没有介绍哦~")
+    ointroduction = models.TextField(
+        '介绍', null=True, blank=True, default="这里暂时没有介绍哦~")
     otype = models.ForeignKey(
         OrganizationType, on_delete=models.CASCADE)
     avatar = models.ImageField(upload_to=f'avatar/', blank=True)
@@ -210,7 +233,8 @@ class Position(models.Model):
 
 
 class Course(models.Model):
-    cid = models.OneToOneField(to=Organization, on_delete=models.CASCADE, related_name="cid")
+    cid = models.OneToOneField(
+        to=Organization, on_delete=models.CASCADE, related_name="cid")
     # cid = models.ForeignKey(Organization, to_field="oid",
     #                        related_name='cid', on_delete=models.CASCADE, primary_key=True)
     # 不应该有这个字段了,这个字段应该自然的就是“组织的名字”
@@ -271,8 +295,10 @@ class Activity(models.Model):
 
 # modified by Kinnuch & genuine
 class TransferRecord(models.Model):
-    proposer = models.ForeignKey(User, related_name='proposer_id', on_delete=models.CASCADE)
-    recipient = models.ForeignKey(User, related_name='recipient_id', on_delete=models.CASCADE)
+    proposer = models.ForeignKey(
+        User, related_name='proposer_id', on_delete=models.CASCADE)
+    recipient = models.ForeignKey(
+        User, related_name='recipient_id', on_delete=models.CASCADE)
     amount = models.FloatField('转账元气值数量', default=0)
     time = models.DateTimeField('转账时间', auto_now_add=True)
     message = models.CharField("备注信息", max_length=255, default='')
