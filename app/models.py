3261b10053ed2f68583d543507d82b5da85cce4c
qingzhenduyu <2280936809@qq.com>
2021-07-24 16:34:56 +0800

    real ultimate lauch_activity

--- app/models.py
+++ app/models.py
@@ -223,14 +223,14 @@ class Activity(models.Model):
     organization_id = models.ForeignKey(
         Organization, to_field="organization_id", related_name="actoid", on_delete=models.CASCADE
     )
-    year = models.IntegerField("活动年份", default=int(datetime.now().strftime("%Y")))
+    year = models.IntegerField("活动年份", default=int(local_dict["semester_data"]["year"]))
     semester = models.CharField("活动学期", choices=Semester.choices, max_length=15)
     publish_time = models.DateTimeField("信息发布时间", blank=True,
-                                        default=datetime.now().strftime('%Y-%m-%d %H:%M:%S'))  # 可以为空
-    sign_start = models.DateTimeField("报名开始时间", blank=True, default=datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
-    sign_end = models.DateTimeField("报名结束时间", blank=True, default=datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
-    start = models.DateTimeField("活动开始时间", blank=True, default=datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
-    end = models.DateTimeField("活动结束时间", blank=True, default=datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+                                        default=datetime.now())  # 可以为空
+    sign_start = models.DateTimeField("报名开始时间", blank=True, default=datetime.now())
+    sign_end = models.DateTimeField("报名结束时间", blank=True, default=datetime.now())
+    start = models.DateTimeField("活动开始时间", blank=True, default=datetime.now())
+    end = models.DateTimeField("活动结束时间", blank=True, default=datetime.now())
 
     location = models.CharField("活动地点", blank=True, max_length=200)
     content = models.CharField("活动内容", max_length=225, blank=True)
