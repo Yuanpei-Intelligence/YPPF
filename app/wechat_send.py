f647cf45c78a45ef3dd7ae0c43055847deef64ab
panhongtao <59553164+panhongtao07@users.noreply.github.com>
2021-07-24 11:31:17 +0800

    更新wechat_send，修改USE_TZ
    更新wechat_send的统一设置，增加注释；对（之前修改的）USE_TZ改动意义进行注释

--- app/wechat_send.py
+++ app/wechat_send.py
@@ -1,31 +1,39 @@
-from datetime import datetime, timedelta
-import random, requests  # 发送验证码
-from app.models import (
-    NaturalPerson,
-    Position,
-    Organization,
-    OrganizationType,
-    Position,
-    Activity,
-    TransferRecord,
-    Paticipant,
-)
-from boottest import local_dict, settings
-from app.utils import MyMD5PasswordHasher, MySHA256Hasher
+import requests
 import json
-login_url = local_dict["url"]["login_url"]
-wechat_url = local_dict["url"]["wechat_url"]
-email_url = local_dict["url"]["email_url"]
-hash_coder = MySHA256Hasher(local_dict["hash"]["base_hasher"])
-wechat_coder = MySHA256Hasher(local_dict["hash"]["wechat"])
-email_coder = MySHA256Hasher(local_dict["hash"]["email"])
-'''
+
+# 设置
+from django.conf import settings
+from boottest import local_dict
+
+# 模型与加密模型
+from app.models import NaturalPerson, Activity
+from app.utils import MyMD5PasswordHasher, MySHA256Hasher
+
+# 日期与定时任务
+from datetime import datetime, timedelta
 from apscheduler.schedulers.background import BackgroundScheduler
 from django_apscheduler.jobstores import DjangoJobStore
 
-scheduler = BackgroundScheduler()
-scheduler.add_jobstore(DjangoJobStore(), "default")
-'''
+# 全局设置
+# 是否启用定时任务，请最好仅在服务器启用，如果不启用，后面的多个设置也会随之变化
+USE_SCHEDULER = False
+# 是否多线程发送，必须启用scheduler，如果启用则发送时无需等待
+USE_MULTITHREAD = True if USE_SCHEDULER else False
+# 决定单次连接的超时时间，响应时间一般为1s或12s（偶尔），建议考虑前端等待时间放弃12s
+TIMEOUT = 15 if USE_MULTITHREAD else 5 or 3.05 or 12 or 15
+# 订阅系统的发送量非常大，不建议重发，因为发送失败之后短时间内重发大概率也会失败
+# 如果未来实现重发，应在base_send_wechat中定义作为参数读入，现在提供了几句简单的代码
+RETRY = False
+
+if USE_SCHEDULER:
+    scheduler = BackgroundScheduler()
+    scheduler.add_jobstore(DjangoJobStore(), "default")
+
+# 全局变量 用来发送和确认默认的导航网址
+default_url = settings.LOGIN_URL
+wechat_url = local_dict["url"]["wechat_url"]
+wechat_coder = MySHA256Hasher(local_dict["hash"]["wechat"])
+
 def base_send_wechat(users, message, card=True, url=None, btntxt=None, default=True):
     '''底层实现发送到微信，是为了方便设置定时任务'''
     post_data = {
@@ -37,7 +45,7 @@ def base_send_wechat(users, message, card=True, url=None, btntxt=None, default=T
     if card:
         post_data["card"] = True
         if default:
-            post_data["url"] = url if url is not None else login_url
+            post_data["url"] = url if url is not None else default_url
             post_data["btntxt"] = btntxt if btntxt is not None else "详情"
         else:
             if url is not None:
@@ -45,24 +53,30 @@ def base_send_wechat(users, message, card=True, url=None, btntxt=None, default=T
             if btntxt is not None:
                 post_data["btntxt"] = btntxt
     post_data = json.dumps(post_data)
+    # if not RETRY or not retry_times:
+    #     retry_times = 1
+    # for i in range(retry_times):
     try:
         failed = users
         errmsg = "连接api失败"
-        response = requests.post(wechat_url, post_data, timeout=15)
+        response = requests.post(wechat_url, post_data, timeout=TIMEOUT)
         response = response.json()
-        if response["status"] == 200:
+        if response["status"] == 200:           # 全部发送成功
             return
-        elif response["data"].get("detail"):
+        elif response["data"].get("detail"):    # 部分发送失败
             errinfos = response["data"]["detail"]
             failed = [x[0] for x in errinfos]
-            errmsg = errinfos[0][1]
+            errmsg = errinfos[0][1]             # 失败原因基本相同，取一个即可
         elif response.get("errMsg"):
-            errmsg = response["errMsg"]
+            errmsg = response["errMsg"]         # 参数等其他传入格式问题
+        # users = failed    # 如果允许重发，则向失败用户重发
         raise OSError
     except:
+        # print(f"第{i+1}次尝试")
         print(f"向企业微信发送失败：失败用户：{failed[:3]}等{len(failed)}人，主要失败原因：{errmsg}")
 
-def publish_activity(aid):
+def publish_activity(aid, only_activated=False):
+    '''根据活动id（实际是主键）向所有订阅该组织信息的学生发送，可以只发给在校学生'''
     try:
         activity = Activity.objects.get(pk=aid)
     except:
@@ -70,6 +84,8 @@ def publish_activity(aid):
         return False
     org = activity.organization_id
     subcribers = org.subscribers                # flat=True时必须只有一个键
+    if only_activated:
+        subcribers = subcribers.exclude(status=NaturalPerson.GraduateStatus.GRADUATED)
     subcribers = list(subcribers.values_list("person_id__username", flat=True))
     num = len(subcribers)
     start, finish = activity.start, activity.finish
@@ -79,8 +95,8 @@ def publish_activity(aid):
         timeformat = "%Y年%m月%d日 %H:%M"   # 显示具体年份
     start = start.strftime(timeformat)
     finish = finish.strftime(timeformat)
-    if len(activity.content) < 120:
-        kws = {"card" : True}
+    if len(activity.content) < 120:         # 卡片类型消息最多显示256字（512字节）
+        kws = {"card" : True}               # 因留白等原因，内容120字左右就超出了
         message = "\n".join((
             activity.topic,
             f"组织者：{activity.organization_id.oname}",
@@ -92,7 +108,7 @@ def publish_activity(aid):
         if activity.URL:
             kws["url"] = activity.URL
             kws["btntxt"] = "阅读原文"
-    else:
+    else:                                   # 超出卡片字数范围的消息使用文本格式发送
         kws = {"card" : False}
         message = "\n".join((
             activity.topic,
@@ -107,15 +123,16 @@ def publish_activity(aid):
         if activity.URL:
             message += f"\n<a href=\"{activity.URL}\">阅读原文</a>"
         else:
-            message += f"\n<a href=\"{settings.LOGIN_URL}\">点击查看详情</a>"
+            message += f"\n<a href=\"{default_url}\">点击查看详情</a>"
     for i in range(0, num, 500):
-        userids = subcribers[i:i+500]
-        '''
-        scheduler.add_job(base_send_wechat, 'date', 
-            args=(userids, message),
-            kwargs=kws,
-            next_run_time=datetime.now() + timedelta(seconds=5+i/100)
-            )
-        '''
-        base_send_wechat(userids, message, **kws) #不使用定时任务请改为这句
+        userids = subcribers[i:i+500]   # 一次最多接受1000个，方便传送只发500个
+        if USE_MULTITHREAD:
+            # 多线程
+            scheduler.add_job(base_send_wechat, 'date', 
+                args=(userids, message),
+                kwargs=kws,
+                next_run_time=datetime.now() + timedelta(seconds=5+i/100)
+                )
+        else:
+            base_send_wechat(userids, message, **kws) # 不使用定时任务请改为这句
     return True
