ac2fa4593cfa3ee06797afa7158625c32c0ed5fa
sunyb-dot <18211195741@126.com>
2021-07-25 18:00:05 +0800

    change position_list to pos_list

--- app/views.py
+++ app/views.py
@@ -571,6 +571,8 @@ def search(request):
             支持通过公开关系的个人搜索组织，即如果某自然人用户可以被上面的人员搜索检出，
             而且该用户选择公开其与组织的关系，那么该组织将在搜索界面呈现。
             搜索结果的呈现内容见organization_field
+        搜索活动
+            支持通过活动名、组织来搜索活动。只要可以搜索到组织，组织对应的活动就也可以被搜到
     """
     try:
         valid, user_type, html_display = utils.check_user_type(request)
@@ -618,14 +620,16 @@ def search(request):
 
         # 搜索组织
         # 先查找通过个人关联到的position_list
-        position_list = Position.objects.activated().filter(Q(person__in=people_list) & Q(show_post=True))
+        pos_list = Position.objects.activater().filter(Q(person__in=people_list) & Q(show_post=True))
         # 通过组织名、组织类名、个人关系查找
         organization_list = Organization.objects.filter(
-            Q(oname__icontains=query) | Q(otype__otype_name__icontains=query) | Q(org__in = position_list.values('org')))
+            Q(oname__icontains=query) | Q(otype__otype_name__icontains=query) | Q(org__in = pos_list.values('org')))
 
         # 组织要呈现的具体内容
         organization_field = ["组织名", "组织类型", "负责人", "近期活动"]
 
+        # 搜索活动
+        
         return render(request, "search.html", locals())
     except Exception as e:
         print(str(e))
