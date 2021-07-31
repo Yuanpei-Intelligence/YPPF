90ee3e3cfa0c3066103b9f3b9617642d539f8c9b
sunyb-dot <18211195741@126.com>
2021-07-31 17:05:55 +0800

    按月发放元气值

--- app/scheduler_func.py
+++ app/scheduler_func.py
@@ -1,7 +1,7 @@
 from apscheduler.schedulers.background import BackgroundScheduler
 from django_apscheduler.jobstores import DjangoJobStore, register_events, register_job
 
-
+from django.db.models import F
 from django.http import JsonResponse, HttpResponse  # Json响应
 from django.shortcuts import render, redirect  # 网页render & redirect
 from django.urls import reverse
@@ -22,28 +22,33 @@ def test():
 '''
 
 
-@register_job(scheduler, 'cron', id='monthly_distribute_YQPoint', day=31, hour=16, minute=2, second=55)
+@register_job(scheduler, 'cron', id='monthly_distribute_YQPoint', day=31, hour=17, minute=3, second=55)
 def monthly_distribute_YQPoint():
     try:
         distributer = Scheduled_YQPoint_Distribute.objects.get(type=Scheduled_YQPoint_Distribute.Schedule_Type.MONTH,
             status=Scheduled_YQPoint_Distribute.Distribute_Status.Yes)
-        print(distributer)
         
     except Exception as e:
         print("\n按月发放元气值失败，原因可能是没有状态为YES或者有多个状态为YES的发放实例\n" + str(e))
 
     # 没有问题，找到要发放元气值的人和组织
     per_to_dis = NaturalPerson.objects.activated().filter(
-        YQPoint__lte=distributer.per_max_YQPoint)
+        YQPoint__lte=distributer.per_max_dis_YQPoint)
     org_to_dis = Organization.objects.activated().filter(
-        YQPoint__lte=distributer.org_max_YQPoint).exclude(oname="元培学院")
-    print(org_to_dis)
-
-    # 计算发放给每个人/组织的多少
+        YQPoint__lte=distributer.org_max_dis_YQPoint).exclude(oname="元培学院")
+    
+    # 计算发放给每个人/组织的多少，进行更改，写入数据库
     college = Organization.objects.get(oname="元培学院")
-    per_get_YQPoint = distributer.per_YQPoint_rate * \
-        college.YQPoint / per_to_dis.count()
-    org_get_YQPoint = distributer.org_YQPoint_rate * \
-        college.YQPoint / org_to_dis.count()
+    if per_to_dis:
+        per_get_YQPoint = distributer.per_YQPoint_rate * \
+            college.YQPoint / per_to_dis.count()
+        per_to_dis.update(YQPoint=F('YQPoint') + per_get_YQPoint)
+        college.YQPoint -= college.YQPoint * distributer.per_YQPoint_rate
+    if org_to_dis:
+        org_get_YQPoint = distributer.org_YQPoint_rate * \
+            college.YQPoint / org_to_dis.count()
+        org_to_dis.update(YQPoint=F('YQPoint') + org_get_YQPoint)
+        college.YQPoint -= college.YQPoint * distributer.org_YQPoint_rate
 
-    # 更改并写入数据库
+    college.save()
+    # print(per_to_dis.values('YQPoint')), for debug
