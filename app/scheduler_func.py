e5a0f4f2108679afe2279ae9d1c02ee270d61294
sunyb-dot <18211195741@126.com>
2021-08-04 19:38:31 +0800

    还需要提高效率

--- app/scheduler_func.py
+++ app/scheduler_func.py
@@ -1,4 +1,5 @@
 from apscheduler.schedulers.background import BackgroundScheduler
+from django.dispatch.dispatcher import receiver
 from django_apscheduler.jobstores import DjangoJobStore, register_events, register_job
 
 from django.db.models import F
@@ -8,47 +9,89 @@ from django.urls import reverse
 from datetime import datetime, timedelta, timezone, time, date
 from django.db import transaction  # 原子化更改数据库
 
-from app.models import Organization, NaturalPerson, Scheduled_YQPoint_Distribute
+from app.models import Organization, NaturalPerson, Scheduled_YQPoint_Distribute, TransferRecord
+from app.wechat_send import base_send_wechat
 
 # 定时任务生成器
 scheduler = BackgroundScheduler()
 scheduler.add_jobstore(DjangoJobStore(), "default")
 
-'''
-@register_job(scheduler, 'cron', id='test', day=1, hour=15, minute=23)
-def test():
-    # 具体要执行的代码
-    print("\n\n\nthis is a test register job!\n\n\n")
-'''
+# @register_job(scheduler, 'cron', id='test', day=1, hour=15, minute=23)
+# def test():
+#     # 具体要执行的代码
+#     print("\n\n\nthis is a test register job!\n\n\n")
 
 
-@register_job(scheduler, 'cron', id='monthly_distribute_YQPoint', day=31, hour=17, minute=3, second=55)
-def monthly_distribute_YQPoint():
+@register_job(scheduler, 'interval', id='weekly_distribute_YQPoint', weeks=0, seconds=2)
+def scheduled_distribute_YQPoint():
     try:
-        distributer = Scheduled_YQPoint_Distribute.objects.get(type=Scheduled_YQPoint_Distribute.Schedule_Type.MONTH,
-            status=Scheduled_YQPoint_Distribute.Distribute_Status.Yes)
-        
-    except Exception as e:
-        print("\n按月发放元气值失败，原因可能是没有状态为YES或者有多个状态为YES的发放实例\n" + str(e))
+        distributer = Scheduled_YQPoint_Distribute.objects.get(type=Scheduled_YQPoint_Distribute.Schedule_Type.WEEK,
+                                                               status=Scheduled_YQPoint_Distribute.Distribute_Status.Yes)
 
+    except Exception as e:
+        print("\n按周发放元气值失败，原因可能是没有状态为YES或者有多个状态为YES的发放实例\n" + str(e))
+    trans_time = datetime.now()
+    time_record = datetime.now()
     # 没有问题，找到要发放元气值的人和组织
     per_to_dis = NaturalPerson.objects.activated().filter(
         YQPoint__lte=distributer.per_max_dis_YQPoint)
     org_to_dis = Organization.objects.activated().filter(
         YQPoint__lte=distributer.org_max_dis_YQPoint).exclude(oname="元培学院")
-    
-    # 计算发放给每个人/组织的多少，进行更改，写入数据库
-    college = Organization.objects.get(oname="元培学院")
-    if per_to_dis:
-        per_get_YQPoint = distributer.per_YQPoint_rate * \
-            college.YQPoint / per_to_dis.count()
-        per_to_dis.update(YQPoint=F('YQPoint') + per_get_YQPoint)
-        college.YQPoint -= college.YQPoint * distributer.per_YQPoint_rate
-    if org_to_dis:
-        org_get_YQPoint = distributer.org_YQPoint_rate * \
-            college.YQPoint / org_to_dis.count()
-        org_to_dis.update(YQPoint=F('YQPoint') + org_get_YQPoint)
-        college.YQPoint -= college.YQPoint * distributer.org_YQPoint_rate
+    # 由学院账号给大家发放
+    YPcollege = Organization.objects.get(oname="元培学院")
 
-    college.save()
-    # print(per_to_dis.values('YQPoint')), for debug
+    print(f"阶段1用时：{(datetime.now()-time_record).seconds}s,{(datetime.now()-time_record).microseconds}ms")
+    time_record = datetime.now()
+    # 计算发放给每个人/组织的多少，进行更改，写入数据库
+
+    try:
+        assert YPcollege.YQPoint >= (per_to_dis.count(
+        ) * distributer.per_YQPoints) + (org_to_dis.count() * distributer.org_YQPoints)
+    except Exception as e:
+        print("\n按周发放元气值失败，原因可能是学院账号的元气值剩余不足\n" + str(e))
+
+    per_to_dis.update(YQPoint=F('YQPoint') + distributer.per_YQPoints)
+    YPcollege.YQPoint -= per_to_dis.count() * distributer.per_YQPoints
+    org_to_dis.update(YQPoint=F('YQPoint') + distributer.org_YQPoints)
+    YPcollege.YQPoint -= org_to_dis.count() * distributer.org_YQPoints
+    YPcollege.save()
+
+    print(f"阶段2用时：{(datetime.now()-time_record).seconds}s,{(datetime.now()-time_record).microseconds}ms")
+    time_record = datetime.now()
+
+    trans_msg = "haoye!!!"
+    
+    # 添加转账记录
+    transfer_record_lst = []
+    for person in per_to_dis:
+        transfer_record_lst.append(TransferRecord(
+            proposer=YPcollege.organization_id,
+            recipient=person.person_id,
+            amount=distributer.per_YQPoints,
+            start_time=trans_time,
+            finish_time=trans_time,
+            message=trans_msg,
+            status=TransferRecord.TransferStatus.ACCEPTED
+        )
+        )
+    for org in org_to_dis:
+        transfer_record_lst.append(TransferRecord(
+            proposer=YPcollege.organization_id,
+            recipient=org.organization_id,
+            amount=distributer.org_YQPoints,
+            start_time=trans_time,
+            finish_time=trans_time,
+            message=trans_msg,
+            status=TransferRecord.TransferStatus.ACCEPTED
+        )
+        )
+    print(f"阶段3用时：{(datetime.now()-time_record).seconds}s,{(datetime.now()-time_record).microseconds}ms")
+    time_record = datetime.now()
+
+    
+    TransferRecord.objects.bulk_create(transfer_record_lst)
+    end_time = datetime.now()
+
+    
+    debug_msg = f"已向{per_to_dis.count()}个自然人和{org_to_dis.count()}个组织转账，用时{(end_time - trans_time).seconds}s,{(end_time - trans_time).microseconds}microsecond\n"
+    print(debug_msg)
