bbee043446e071b1598cad07806460e60f3ee9c3
Kinnuch <59094117+Kinnuch@users.noreply.github.com>
2021-08-16 17:44:11 +0800

    Update settings.py

--- boottest/settings.py
+++ boottest/settings.py
@@ -102,7 +102,7 @@ DATABASES = {
         "HOST": "127.0.0.1",
         "PORT": 3306,
         "USER": local_dict["database"]["USER"],
-        "PASSWORD": local_dict["database"]["PASSWORD"],,
+        "PASSWORD": local_dict["database"]["PASSWORD"],
         # 'OPTIONS': {
         #     "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
         # }
