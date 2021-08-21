0876fc14634455a4986f02b5be5b49ba211fd5b2
qingzhenduyu <2280936809@qq.com>
2021-08-21 23:22:14 +0800

    daily pull

--- app/models.py
+++ app/models.py
@@ -665,11 +665,7 @@ class NewOrganization(CommentBase):
         CONFIRMED = (1, "主管老师已同意")  # 审过同意
         TOBEMODIFIED = (2, "需要修改")
         CANCELED = (3, "已取消")  # 老师不同意或者发起者取消
-<<<<<<< HEAD
         REFUSED=(4,"已拒绝")
-=======
-        REFUSED = (4, "已拒绝")
->>>>>>> 218cc088c1d107d09f15411502c261aeee53819c
 
     status = models.SmallIntegerField(choices=NewOrgStatus.choices, default=0)
     
@@ -706,11 +702,7 @@ class Reimbursement(CommentBase):
         # 如果需要更多审核，每个审核的确认状态应该是2的幂
         # 根据最新要求，最终不以线上为准，不再设置转账状态
         CANCELED = (4, "已取消")
-<<<<<<< HEAD
         REFUSED = (5,"已拒绝")
-=======
-        REFUSED = (5, "已拒绝")
->>>>>>> 218cc088c1d107d09f15411502c261aeee53819c
 
     activity = models.ForeignKey(
         Activity, related_name="reimbursement", on_delete=models.CASCADE
