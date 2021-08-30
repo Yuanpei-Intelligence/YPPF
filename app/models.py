e032284cce8c73a5746525b53ce29b80bbab43bc
qingzhenduyu <56719355+qingzhenduyu@users.noreply.github.com>
2021-08-30 23:39:24 +0800

    报销增加转账逻辑 (#209)
    * 新建组织信息为空的issue
    
    * comment
    
    * comment
    
    * bugs
    
    * issue 141
    
    * 组织类型
    
    * bugs
    
    * daily pull
    
    * bugs
    
    * 检查重名逻辑重写
    
    * 检查重名逻辑重写
    
    * before pull
    
    * daily pull
    
    * 新建组织整合
    
    * 评论通知
    
    * bugs
    
    * bugs
    
    * before pull
    
    * 报销逻辑整合
    
    * 报销逻辑整合
    
    * 后端部分处理
    
    * 通知修改
    
    * daily pull +大bug
    
    * before pull
    
    * 修复被人新加了一次notification_status_change调用的bug
    
    * 前端大改
    
    * before pull
    
    * before pull
    
    * 评论区头像加成
    
    * 评论区头像加成
    
    * bugs修复
    
    * 希望是最后一次修改报销了
    
    * test
    
    * 新建报销逻辑重改
    
    * daily before pull
    
    * before pull
    
    * small bug
    
    * new_submit成功后，可继续申请
    
    * 活动展示页面对报销的适配
    
    * 报销的活动链接的一点点小bug
    
    * 状态徽章+取消二次确认
    
    * 报销元气值扣除逻辑修改
    
    * 新建组织人事徽章
    
    * 报销中增加转账逻辑
    
    * 报销中增加转账逻辑
    
    * 修改字段

--- app/models.py
+++ app/models.py
@@ -659,7 +659,7 @@ class TransferRecord(models.Model):
         REFUND = (4, "已退回")
 
     status = models.SmallIntegerField(choices=TransferStatus.choices, default=1)
-
+    is_increase=models.IntegerField("报销兑换",default=0,help_text="报销时转账并未实质发生，用此字段标识，0标识默认转账，1为报销兑换")
     def save(self, *args, **kwargs):
         self.amount = round(self.amount, 1)
         super(TransferRecord, self).save(*args, **kwargs)
@@ -969,7 +969,7 @@ class Reimbursement(CommentBase):
     message = models.TextField("备注信息", default="", blank=True)
     pos = models.ForeignKey(User, on_delete=models.CASCADE)
     status = models.SmallIntegerField(choices=ReimburseStatus.choices, default=0)
-
+    record=models.ForeignKey(TransferRecord,on_delete=models.CASCADE)#转账信息的记录
     def __str__(self):
         return f'{self.related_activity.title}活动报销'
         
