8261d44385b6fa53210b5452402e51fe86ea6e6b
qingzhenduyu <2280936809@qq.com>
2021-09-01 11:00:20 +0800

    test

--- app/views.py
+++ app/views.py
@@ -759,7 +759,6 @@ def homepage(request):
         dictmp["deadline"] = deadline
         dictmp["act"] = act
         signup_list.append(dictmp)
-        dictmp = {}
     signup_list.sort(key=lambda x:x["deadline"])
     signup_list=signup_list[:10]
     # 如果提交了心愿，发生如下的操作
