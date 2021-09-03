d0f05f3b2761dd4d850395a898d28ce222acb3bd
panhongtao <59553164+panhongtao07@users.noreply.github.com>
2021-09-03 17:50:55 +0800

    修改定时任务启动位置

--- app/scheduler.py
+++ app/scheduler.py
@@ -1,6 +1,8 @@
 from apscheduler.schedulers.background import BackgroundScheduler
 from django.dispatch.dispatcher import receiver
-from django_apscheduler.jobstores import DjangoJobStore, register_events, register_job
+from django_apscheduler.jobstores import DjangoJobStore
+# from django_apscheduler.jobstores import register_events, register_job
+# 仅在django-apscheduler的0.4以下（不含）版本中才可以使用这些函数，高版本不兼容，不要用
 
 # 定时任务生成器
 scheduler = BackgroundScheduler()
