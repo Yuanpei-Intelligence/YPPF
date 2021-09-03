78640d7f6b38621c815a3147b2edba3c2c3bdf57
panhongtao <59553164+panhongtao07@users.noreply.github.com>
2021-09-03 18:16:08 +0800

    修改定时任务兼容问题

--- app/scheduler.py
+++ app/scheduler.py
@@ -3,6 +3,7 @@ from django.dispatch.dispatcher import receiver
 from django_apscheduler.jobstores import DjangoJobStore
 # from django_apscheduler.jobstores import register_events, register_job
 # 仅在django-apscheduler的0.4以下（不含）版本中才可以使用这些函数，高版本不兼容，不要用
+# register_job(scheduler, ...)的替代写法为scheduler.scheduled_job(...)
 
 # 定时任务生成器
 scheduler = BackgroundScheduler()
