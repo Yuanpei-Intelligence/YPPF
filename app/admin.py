260886a57971f845dcc646ab23d469a0f1d73722
panhongtao <59553164+panhongtao07@users.noreply.github.com>
2021-09-07 23:12:42 +0800

    修改后台

--- app/admin.py
+++ app/admin.py
@@ -1,10 +1,18 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 from app.models import *
 
 # Register your models here.
+admin.site.site_title = '元培成长档案管理后台'
+admin.site.site_header = '元培成长档案 - 管理后台'
+
+
+@admin.register(NaturalPerson)
 class NaturalPersonAdmin(admin.ModelAdmin):
     fieldsets = (
-        ["Commom Attributes", {"fields": ("person_id", "name", "gender", "identity", "YQPoint")}],
+        ["Commom Attributes", {"fields": (
+            "person_id", "name", "nickname", "gender", "identity",
+            "YQPoint", "YQPoint_Bonus", "wechat_receive_level")}],
         [
             "Student Attributes",
             {
@@ -19,9 +27,12 @@ class NaturalPersonAdmin(admin.ModelAdmin):
         "person_id",
         "name",
         "identity",
+        "first_time_login",
     ]
     search_fields = ("person_id__username", "name")
+    list_filter = ("stu_grade", "status", "identity", "first_time_login", "wechat_receive_level")
 
+@admin.register(Freshman)
 class FreshmanAdmin(admin.ModelAdmin):
     list_display = [
         "sid",
@@ -33,25 +44,54 @@ class FreshmanAdmin(admin.ModelAdmin):
     search_fields = ("sid", "name")
     list_filter = ("status", "grade", "place")
 
+
+@admin.register(OrganizationType)
+class OrganizationTypeAdmin(admin.ModelAdmin):
+    list_display = ["otype_id", "otype_name", "incharge", "job_name_list", "control_pos_threshold"]
+    search_fields = ("oname", "otype", "incharge__name", "job_name_list")
+
+
+@admin.register(Organization)
 class OrganizationAdmin(admin.ModelAdmin):
-    list_display = ["oname"]
-    search_fields = ("oname",)
+    list_display = ["organization_id", "oname", "otype", "Managers"]
+    search_fields = ("oname", "otype")
+    list_filter = ("otype", )
+
+    def Managers(self, obj):
+        display = ''
+        all_pos = sorted(Position.objects.activated().filter(
+                org=obj, is_admin=True).values_list(
+                    'pos', flat=True).distinct())
+        for pos in all_pos:
+            managers = Position.objects.activated().filter(
+                org=obj, pos=pos, is_admin=True)
+            if managers:
+                display += f'{obj.otype.get_name(pos)}：'
+                names = managers.values_list('person__name', flat=True)
+                display += f"<li>{'、'.join(names)}</li>"
+        if not display:
+            display = '暂无'
+        return mark_safe(display)
+    Managers.short_description = "管理者"
 
 
+@admin.register(Position)
 class PositionAdmin(admin.ModelAdmin):
-    list_display = ["person", "org", "pos"]
-    search_fields = ("person__name", "org__oname", "pos")
+    list_display = ["person", "org", "pos", "pos_name", "is_admin"]
+    search_fields = ("person__name", "org__oname")
+    list_filter = ('pos', 'is_admin')
+    list_display_links = ('person', 'org')
+
+    def pos_name(self, obj):
+        return obj.org.otype.get_name(obj.pos)
+    pos_name.short_description = "职位名称"
 
 
-admin.site.register(NaturalPerson, NaturalPersonAdmin)
-admin.site.register(Freshman, FreshmanAdmin)
-admin.site.register(Organization, OrganizationAdmin)
-admin.site.register(Position, PositionAdmin)
 
-admin.site.register(OrganizationType)
 admin.site.register(Activity)
 admin.site.register(TransferRecord)
 
 admin.site.register(YQPointDistribute)
 admin.site.register(Notification)
 admin.site.register(Help)
+admin.site.register(ModifyRecord)
