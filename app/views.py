8140f37227e2e987894a9e8f30104787fbc5273f
panhongtao <59553164+panhongtao07@users.noreply.github.com>
2021-09-07 11:54:53 +0800

    批量创建性能优化

--- app/views.py
+++ app/views.py
@@ -4009,11 +4009,13 @@ def send_message_check(me, request):
 
     try:
         if receiver_type == "订阅用户":
-            receivers = NaturalPerson.objects.exclude(id__in=me.unsubscribers.all())
+            receivers = NaturalPerson.objects.exclude(
+                id__in=me.unsubscribers.all()).select_related('person_id')
             receivers = [receiver.person_id for receiver in receivers]
         else:   # 检查过逻辑了，不可能是其他的
             receivers = NaturalPerson.objects.filter(
-                id__in=me.position_set.values_list('person_id', flat=True))
+                id__in=me.position_set.values_list('person_id', flat=True)
+                ).select_related('person_id')
             receivers = [receiver.person_id for receiver in receivers]
 
         # 创建通知
