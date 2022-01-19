807432d3e89874e8b033b8c97f2d22e373225c65
qingzhenduyu <2280936809@qq.com>
2022-01-19 22:05:54 +0800

    data import

--- app/views.py
+++ app/views.py
@@ -65,13 +65,13 @@ from django.db import transaction
 from django.db.models import Q, F
 from django.contrib.auth.password_validation import CommonPasswordValidator, NumericPasswordValidator
 from django.core.exceptions import ValidationError
-"""
+
 
 # 定时任务不在views直接调用
 # 但是天气任务还是在这里弄吧，太奇怪了
 from app.scheduler_func import start_scheduler
 start_scheduler(with_scheduled_job=True, debug=True)
-"""
+
 
 email_url = local_dict["url"]["email_url"]
 hash_coder = MySHA256Hasher(local_dict["hash"]["base_hasher"])
