5c2483d7b9cea733808f258d7ccb660072cd073a
qianweinan <ypqwn@stu.pku.edu.cn>
2022-01-26 16:29:22 +0800

    修改app.views，废除在地下室URL中增加用户信息的方式

--- app/views.py
+++ app/views.py
@@ -172,22 +172,24 @@ def index(request):
 
             is_underground, arg_origin = utils.get_std_underground_url(arg_origin)
             is_inner, arg_origin = utils.get_std_inner_url(arg_origin)
-            if is_inner and not is_underground:
-                return redirect(arg_origin)  # 是内部链接且不是地下室链接
-            if not is_underground:
-                return redirect(arg_origin)  #实际上只要不是地下室链接就行
-
-            timeStamp = str(int(datetime.utcnow().timestamp())) # UTC 统一服务器
-            username = request.user.username    # session["username"] 已废弃
-            en_pw = hash_coder.encode(username + timeStamp)
-            try:
-                userinfo = NaturalPerson.objects.get(person_id__username=username)
-                arg_origin = append_query(arg_origin,
-                    Sid=username, timeStamp=timeStamp, Secret=en_pw, name=userinfo.name)
-            except:
-                arg_origin = append_query(arg_origin,
-                    Sid=username, timeStamp=timeStamp, Secret=en_pw)
-            return redirect(arg_origin)
+            if is_inner: # and not is_underground:
+                return redirect(arg_origin)  # 是内部链接
+            # TODO: task qwn 2022-1-26: 注释掉下面部分后运行可能出错，是因为url中不再有用户信息，
+            #                           需要在Appointment.views中改变获取用户信息的方式。
+            # if not is_underground:
+            #     return redirect(arg_origin)  #实际上只要不是地下室链接就行
+            
+            # timeStamp = str(int(datetime.utcnow().timestamp())) # UTC 统一服务器
+            # username = request.user.username    # session["username"] 已废弃
+            # en_pw = hash_coder.encode(username + timeStamp)
+            # try:
+            #     userinfo = NaturalPerson.objects.get(person_id__username=username)
+            #     arg_origin = append_query(arg_origin,
+            #         Sid=username, timeStamp=timeStamp, Secret=en_pw, name=userinfo.name)
+            # except:
+            #     arg_origin = append_query(arg_origin,
+            #         Sid=username, timeStamp=timeStamp, Secret=en_pw)
+            # return redirect(arg_origin)
 
     return render(request, "index.html", locals())
 
