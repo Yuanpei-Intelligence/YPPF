d53d0393abcf87001ed057971f3c41b358ad1f2c
xzy <xiongziyin@stu.pku.edu.cn>
2022-01-27 21:34:49 +0800

    Update identity.py
    修改接口函数

--- Appointment/utils/identity.py
+++ Appointment/utils/identity.py
@@ -7,61 +7,94 @@
 可能依赖于app.utils
 '''
 from Appointment.models import Participant
-from django.core.exceptions import ObjectDoesNotExist
 from app.utils import (
-    get_person_or_org,
     check_user_type,
+    get_person_or_org,
+    get_user_ava,
 )
-from app.models import (
-    NaturalPerson,
-    Organization,
-    OrganizationType,
-)
+from typing import Union
+from django.contrib.auth.models import User
+from app.models import *
 
 __all__ = [
+    'user2participant',
+    'is_org',
+    'is_person',
+    'get_name',
+    'get_avatar',
 ]
 
 
 def user2participant(user, update=False):
     '''通过User对象获取对应的参与人对象, noexcept
+
     Args:
     - update: 获取带更新锁的对象, 暂不需要支持
+
     Returns:
     - participant: 满足participant.Sid=user, 不存在时返回`None`
     ''' 
     try:
         return Participant.objects.get(Sid=user)
-    except ObjectDoesNotExist:
+    except:
         return None
 
 
-def user2info(user):
-    '''返回User对象对应的(type, object)二元组'''
+def user2info(user: User):
+    '''返回User对象对应的(object, type)二元组'''
     user_type = check_user_type(user)[1]
     obj = get_person_or_org(user, user_type)
-    return user_type, obj
+    return obj, user_type
 
 
-def participant2obj(participant):
-    '''返回participant对应的object，不存在时返回None'''
-    try:
-        return NaturalPerson.objects.get(person_id=participant.Sid)
-    except ObjectDoesNotExist:
-        return None
+def _arg2user(participant: Union[Participant, User]):
+    '''把范围内的参数转化为User对象'''
+    user = participant
+    if isinstance(user, Participant):
+        user = participant.Sid
+        # TODO: task 2 pht 修改模型字段后删除下行
+        if isinstance(user, str): user = User.objects.get(username=user)
+    return user
 
 
-def request2obj(request):
-    '''返回request对应的object'''
-    username = request.user.get_username()
-    try:
-        obj = NaturalPerson.objects.get(person_id_username=username)
-    except ObjectDoesNotExist:
-        obj = Organization.objects.get(organization_id_username=username)
-    return obj
+# 获取用户身份
+def _is_org_type(usertype):
+    return usertype == "Organization"
+
+def is_org(participant: Union[Participant, User]):
+    '''返回participant对象是否是组织'''
+    user = _arg2user(participant)
+    return _is_org_type(check_user_type(user)[1])
+
+def is_person(participant: Union[Participant, User]):
+    '''返回participant是否是个人'''
+    return not is_org(participant)
 
 
-def request2participant(request):
-    '''返回request对应的participant'''
-    username = request.user.get_username()
-    obj = NaturalPerson.objects.get(person_id_username=username)
-    return Participant.objects.get(Sid=obj.person_id)
+# 获取用户信息
+def _get_userinfo(user: User):
+    '''返回User对象对应的(object, type)二元组'''
+    user_type = check_user_type(user)[1]
+    obj = get_person_or_org(user, user_type)
+    return obj, user_type
+
+
+def get_name(participant: Union[Participant, User]):
+    '''返回participant(个人或组织)的名称'''
+    obj, user_type = _get_userinfo(_arg2user(participant))
+    if _is_org_type(user_type):
+        return obj.oname
+    else:
+        return obj.name
+
+
+def get_avatar(participant: Participant):
+    '''返回participant的头像'''
+    obj, user_type = _get_userinfo(_arg2user(participant))
+    return get_user_ava(obj, user_type)
+
+
+
+
+
+
