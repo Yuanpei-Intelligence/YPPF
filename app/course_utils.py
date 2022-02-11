9acf15a00104b76fdae58135651cd09ddeaff3e6
xpr <xpr820@163.com>
2022-02-11 17:54:54 +0800

    1.格式规范化；2.避免使用ActivityException；3.json中default_examiner放到了course下，用get_setting读json；4.对course_utils.py和course_views.py去掉了冗余部分；5.edit函数中如果修改的活动不是课程活动会抛出错误信息；6.category=1改为了常量。目前版本能跑通。

--- app/course_utils.py
+++ app/course_utils.py
@@ -1,3 +1,4 @@
+from unicodedata import category
 from app.utils_dependency import *
 from app.models import (
     NaturalPerson,
@@ -24,11 +25,12 @@ from datetime import datetime, timedelta
 
 from app.scheduler import scheduler
 
+
 def course_activity_base_check(request):
     '''检查课程活动，是activity_base_check的简化版'''
-
     context = dict()
 
+    # 读取活动名称和地点，检查合法性
     context["title"] = request.POST["title"]
     # context["introduction"] = request.POST["introduction"] # 暂定不需要简介
     context["location"] = request.POST["location"]
@@ -36,14 +38,17 @@ def course_activity_base_check(request):
     # assert len(context["introduction"]) > 0 # 暂定不需要简介
     assert len(context["location"]) > 0
 
-    # 时间
-    act_start = datetime.strptime(request.POST["lesson_start"], "%Y-%m-%d %H:%M")  # 活动开始时间
-    act_end = datetime.strptime(request.POST["lesson_end"], "%Y-%m-%d %H:%M")  # 活动结束时间
+    # 读取活动时间，检查合法性
+    act_start = datetime.strptime(
+        request.POST["lesson_start"], "%Y-%m-%d %H:%M")  # 活动开始时间
+    act_end = datetime.strptime(
+        request.POST["lesson_end"], "%Y-%m-%d %H:%M")  # 活动结束时间
     context["start"] = act_start
     context["end"] = act_end
     assert check_ac_time(act_start, act_end)
 
-    context["need_checkin"] = True # 默认需要签到
+    # 默认需要签到
+    context["need_checkin"] = True
 
     return context
 
@@ -53,50 +58,45 @@ def create_single_course_activity(request):
     创建单次课程活动，是create_activity的简化版
     '''
     context = course_activity_base_check(request)
+
     # 查找是否有类似活动存在
     old_ones = Activity.objects.activated().filter(
         title=context["title"],
         start=context["start"],
         # introduction=context["introduction"], # 暂定不需要简介
-        location=context["location"]
+        location=context["location"],
+        category=Activity.ActivityCategory.COURSE  # 查重时要求是课程活动
     )
-    if len(old_ones) == 0:
-        old_ones = Activity.objects.filter(
-            title = context["title"],
-            start = context["start"],
-            # introduction = context["introduction"], # 暂定不需要简介
-            location = context["location"],
-            status = Activity.Status.REVIEWING,
-        )
     if len(old_ones):
         assert len(old_ones) == 1, "创建活动时，已存在的相似活动不唯一"
         return old_ones[0].id, False
 
+    # 默认审核老师（刘欣悦老师）
+    default_examiner_name = get_setting("course/default_examiner")
     examine_teacher = NaturalPerson.objects.get(
-        name=local_dict['default_course_examiner'], identity=NaturalPerson.Identity.TEACHER)
-    # 检查完毕，创建活动
+        name=default_examiner_name, identity=NaturalPerson.Identity.TEACHER)
+
+    # 创建活动
     org = get_person_or_org(request.user, "Organization")
-
     activity = Activity.objects.create(
-                    title=context["title"],
-                    organization_id=org,
-                    examine_teacher=examine_teacher,
-                    # introduction=context["introduction"],# 暂定不需要简介
-                    location=context["location"],
-                    start=context["start"],
-                    end=context["end"],
-                    category=1,
-                    # capacity, URL, budget, YQPoint, bidding, 
-                    # apply_reason, inner, source, end_before均为default
-                )
-    activity.need_checkin = True # 默认需要签到
+        title=context["title"],
+        organization_id=org,
+        examine_teacher=examine_teacher,
+        # introduction=context["introduction"],# 暂定不需要简介
+        location=context["location"],
+        start=context["start"],
+        end=context["end"],
+        category=Activity.ActivityCategory.COURSE,
+        need_checkin=True  # 默认需要签到
+        # capacity, URL, budget, YQPoint, bidding,
+        # apply_reason, inner, source, end_before均为default
+    )
 
+    # 因为目前没有报名环节，活动状态在活动开始前默认都是WAITING，按预审核活动的逻辑
     activity.recorded = True
-    # 因为目前没有报名环节，活动状态在活动开始前默认都是WAITING
     activity.status = Activity.Status.WAITING
 
     # 让课程小组成员参与本活动
-    
     positions = Position.objects.activated().filter(org=activity.organization_id)
     for p in positions:
         activity.current_participants += 1
@@ -106,24 +106,26 @@ def create_single_course_activity(request):
         )
         participant.status = Participant.AttendStatus.APLLYSUCCESS
         participant.save()
-
     activity.save()
 
+    # 通知课程小组成员
     notifyActivity(activity.id, "newActivity")
 
+    # 引入定时任务：提前15min提醒、活动状态由WAITING变PROGRESSING再变END
     scheduler.add_job(notifyActivity, "date", id=f"activity_{activity.id}_remind",
-        run_date=activity.start - timedelta(minutes=15), args=[activity.id, "remind"], replace_existing=True)
-    # 活动状态修改（初始时是WAITING）
+                      run_date=activity.start - timedelta(minutes=15), args=[activity.id, "remind"], replace_existing=True)
     scheduler.add_job(changeActivityStatus, "date", id=f"activity_{activity.id}_{Activity.Status.PROGRESSING}",
-        run_date=activity.start, args=[activity.id, Activity.Status.WAITING, Activity.Status.PROGRESSING])
+                      run_date=activity.start, args=[activity.id, Activity.Status.WAITING, Activity.Status.PROGRESSING])
     scheduler.add_job(changeActivityStatus, "date", id=f"activity_{activity.id}_{Activity.Status.END}",
-        run_date=activity.end, args=[activity.id, Activity.Status.PROGRESSING, Activity.Status.END])
+                      run_date=activity.end, args=[activity.id, Activity.Status.PROGRESSING, Activity.Status.END])
     activity.save()
 
     # 使用一张默认图片以便viewActivity, examineActivity等页面展示
     tmp_pic = '/static/assets/img/announcepics/1.JPG'
-    ActivityPhoto.objects.create(image=tmp_pic, type=ActivityPhoto.PhotoType.ANNOUNCE, activity=activity)
+    ActivityPhoto.objects.create(
+        image=tmp_pic, type=ActivityPhoto.PhotoType.ANNOUNCE, activity=activity)
 
+    # 通知（刘欣悦）老师审核
     notification_create(
         receiver=examine_teacher.person_id,
         sender=request.user,
@@ -143,91 +145,90 @@ def modify_course_activity(request, activity):
     '''
     修改单次课程活动信息，是modify_activity的简化版
     '''
-    
-    # 是否需要检查活动的status？
-    # if activity.status not in [
-    #     Activity.Status.APPLYING, Activity.Status.WAITING, 
-    #     Activity.Status.REVIEWING
-    # ]:
-    #     raise ValueError
-    
+    # 课程活动无需报名，在开始前都是等待中的状态
+    if activity.status != Activity.Status.WAITING:
+        raise ValueError
+
     context = course_activity_base_check(request)
 
+    # 记录旧信息（以便发通知），写入新信息
     old_title = activity.title
     activity.title = context["title"]
     # activity.introduction = context["introduction"]# 暂定不需要简介
     old_location = activity.location
     activity.location = context["location"]
-    
     old_start = activity.start
     activity.start = context["start"]
     old_end = activity.end
     activity.end = context["end"]
-
     activity.save()
 
     # 目前只要编辑了活动信息，无论活动处于什么状态，都通知全体选课同学
     # if activity.status != Activity.Status.APPLYING and activity.status != Activity.Status.WAITING:
     #     return
-    
-    to_participants = [f"您参与的书院课程活动{old_title}发生变化"] # FIXME: 目前是审核前后都允许修改title，也可以改成都不允许
+
+    # 写通知
+    to_participants = [f"您参与的书院课程活动{old_title}发生变化"]
     if old_title != activity.title:
         to_participants.append(f"活动更名为{activity.title}")
-    if old_location != request.POST["location"]:
-        to_participants.append("活动地点修改为" + request.POST["location"])
+    if old_location != activity.location:
+        to_participants.append(f"活动地点修改为{activity.location}")
+    if old_start != activity.start:
+        to_participants.append(
+            f"活动开始时间调整为{activity.start.strftime('%Y-%m-%d %H:%M')}")
 
-    # 时间改变
-    if activity.start != old_start:
-        to_participants.append(f"活动开始时间调整为{activity.start.strftime('%Y-%m-%d %H:%M')}")
-    
+    # 更新定时任务
     scheduler.add_job(notifyActivity, "date", id=f"activity_{activity.id}_remind",
-        run_date=activity.start - timedelta(minutes=15), args=[activity.id, "remind"], replace_existing=True)
+                      run_date=activity.start - timedelta(minutes=15), args=[activity.id, "remind"], replace_existing=True)
     scheduler.add_job(changeActivityStatus, "date", id=f"activity_{activity.id}_{Activity.Status.PROGRESSING}",
-        run_date=activity.start, args=[activity.id, Activity.Status.WAITING, Activity.Status.PROGRESSING], replace_existing=True)
+                      run_date=activity.start, args=[activity.id, Activity.Status.WAITING, Activity.Status.PROGRESSING], replace_existing=True)
     scheduler.add_job(changeActivityStatus, "date", id=f"activity_{activity.id}_{Activity.Status.END}",
-        run_date=activity.end, args=[activity.id, Activity.Status.PROGRESSING, Activity.Status.END], replace_existing=True)
+                      run_date=activity.end, args=[activity.id, Activity.Status.PROGRESSING, Activity.Status.END], replace_existing=True)
 
+    # 发通知
     notifyActivity(activity.id, "modification_par", "\n".join(to_participants))
 
 
 def cancel_course_activity(request, activity):
+    '''
+    取消单次课程活动，是cancel_activity的简化版，在聚合页面被调用
 
-    if activity.status == Activity.Status.REVIEWING:
-        activity.status = Activity.Status.ABORT
-        activity.save()
-        # 修改老师的通知
-        notification = Notification.objects.get(
-            relate_instance=activity,
-            status=Notification.Status.UNDONE
-        )
-        notification_status_change(notification, Notification.Status.DELETE)
-        return
+    在聚合页面中，应确保activity是课程活动，并且应检查activity.status，
+    如果不是WAITING或PROGRESSING，不应调用本函数
 
+    返回True则表示成功取消，返回False表示"课程活动已于一天前开始，不能取消。"
+    （或者也可以在聚合页面判断出来能不能取消）
+    '''
+    # 只有WAITING和PROGRESSING有可能修改
+    if activity.status != Activity.Status.WAITING and activity.status != Activity.Status.PROGRESSING:
+        raise ValueError
+
+    # 课程活动已于一天前开始则不能取消，这一点也可以在聚合页面进行判断
     if activity.status == Activity.Status.PROGRESSING:
         if activity.start.day == datetime.now().day and datetime.now() < activity.start + timedelta(days=1):
             pass
         else:
-            raise ActivityException("课程活动已于一天前开始，不能取消。")
+            return False
 
-    if activity.status == Activity.Status.CANCELED:
-        raise ActivityException("课程活动已取消。")
-
-    org = Organization.objects.select_for_update().get(
-                organization_id=request.user
-            )
-
+    # 取消活动
     activity.status = Activity.Status.CANCELED
     # 目前只要取消了活动信息，无论活动处于什么状态，都通知全体选课同学
-    notifyActivity(activity.id, "modification_par", 
-    f"您报名的书院课程活动{activity.title}已取消（活动原定开始于{activity.start.strftime('%Y-%m-%d %H:%M')}）。")
+    notifyActivity(activity.id, "modification_par",
+                   f"您报名的书院课程活动{activity.title}已取消（活动原定开始于{activity.start.strftime('%Y-%m-%d %H:%M')}）。")
+
+    # 删除老师的审核通知（如果有）
     notification = Notification.objects.get(
         relate_instance=activity,
         typename=Notification.Type.NEEDDO
     )
     notification_status_change(notification, Notification.Status.DELETE)
 
+    # 取消定时任务
     scheduler.remove_job(f"activity_{activity.id}_remind")
-    scheduler.remove_job(f"activity_{activity.id}_{Activity.Status.PROGRESSING}")
+    scheduler.remove_job(
+        f"activity_{activity.id}_{Activity.Status.PROGRESSING}")
     scheduler.remove_job(f"activity_{activity.id}_{Activity.Status.END}")
 
     activity.save()
+
+    return True
