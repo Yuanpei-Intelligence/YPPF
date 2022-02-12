02ed64e5b6dd477d5f6a3bdb494137483423bc90
Yijia Shao <67158122+EchoShao8899@users.noreply.github.com>
2022-02-12 21:05:22 +0800

    课程小组聚合页面 (#380)
    * Fix a nit.
    
    * Provide a skeleton for viewActivity.
    
    * Backup web page skeleton.
    
    * Add org activity aggregation page.
    
    * modify org_views
    
    * Polish the code.
    
    * Polish naming.
    
    * Enhance the logic of side bar display.
    
    * add filter
    
    * add filter2
    
    * Move the code to course_view.py
    
    * Delete unnecessary import.
    
    * Add TODOs for page linking.
    
    * use activated()
    
    Co-Authored-By: dusixian <1912647548@qq.com>

--- app/urls.py
+++ app/urls.py
@@ -76,6 +76,7 @@ urlpatterns = [
     # 书院课程相关内容
     path("addSingleCourseActivity/", course_views.addSingleCourseActivity, name="addSingleCourseActivity"),
     path("editCourseActivity/<str:aid>", course_views.editCourseActivity, name="editCourseActivity"),
+    path("showCourseActivity/", course_views.showCourseActivity, name="showCourseActivity"),
 ] + [
     # 组织相关操作
     path("saveShowPositionStatus", org_views.saveShowPositionStatus, name="saveShowPositionStatus"),
