b53e3e51b9926bf4f13b08d1682a49965e14d91b
yabukota <wangxiaoyu2001@pku.edu.cn>
2022-02-19 23:26:39 +0800

    删掉多余代码

--- app/urls.py
+++ app/urls.py
@@ -115,7 +115,6 @@ urlpatterns = [
     #      data_import.load_notification_info,
     #      name="load_notification_info"),    #服务器弃用
     path("loadhelp/", data_import.load_help, name="load_help"),
-] + [
     path("loadcourecord/", data_import.load_CouRecord,  name="load_cou_record"),
 ] + [
     # 埋点
