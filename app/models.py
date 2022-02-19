9c923db23d7cec5869b707ac8e3fc24dc09700da
qingzhenduyu <2280936809@qq.com>
2022-02-19 23:49:03 +0800

    test

--- app/models.py
+++ app/models.py
@@ -1425,7 +1425,6 @@ class Course(models.Model):
     objects: CourseManager = CourseManager()
 
     def save(self, *args, **kwargs):
-        self.bidding = round(self.bidding, 1)
         super().save(*args, **kwargs)
 
     def __str__(self):
