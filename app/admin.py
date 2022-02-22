6d43278e4557df3936d4a17ccc74c6d18367c134
panhongtao <59553164+panhongtao07@users.noreply.github.com>
2022-02-23 02:18:08 +0800

    简单整理活动后台

--- app/admin.py
+++ app/admin.py
@@ -340,13 +340,9 @@ class ActivityAdmin(admin.ModelAdmin):
         return f'{obj.current_participants}/{"无限" if obj.capacity == 10000 else obj.capacity}'
     participant_diaplay.short_description = "报名情况"
 
-    actions = [
-                'refresh_count',
-                'to_waiting', 'to_processing', 'to_end',
-                'cancel_scheduler'
-        ]
+    actions = []
 
-    @as_action("更新 报名人数", update=True)
+    @as_action("更新 报名人数", actions, update=True)
     def refresh_count(self, request, queryset):
         for activity in queryset:
             activity.current_participants = Participant.objects.filter(
@@ -368,7 +364,7 @@ class ActivityAdmin(admin.ModelAdmin):
         queryset.update(category=Activity.ActivityCategory.COURSE)
         return self.message_user(request=request, message='修改成功!')
 
-    @as_action("进入 等待中 状态")
+    @as_action("进入 等待中 状态", actions)
     def to_waiting(self, request, queryset):
         if len(queryset) != 1:
             return self.message_user(
@@ -385,7 +381,7 @@ class ActivityAdmin(admin.ModelAdmin):
             return self.message_user(request=request,
                                     message='修改成功!')
     
-    @as_action("进入 进行中 状态")
+    @as_action("进入 进行中 状态", actions)
     def to_processing(self, request, queryset):
         if len(queryset) != 1:
             return self.message_user(
@@ -399,10 +395,9 @@ class ActivityAdmin(admin.ModelAdmin):
             return self.message_user(request=request,
                                     message='修改成功, 并移除了定时任务!')
         except:
-            return self.message_user(request=request,
-                                    message='修改成功!')
+            return self.message_user(request=request, message='修改成功!')
     
-    @as_action("进入 已结束 状态")
+    @as_action("进入 已结束 状态", actions)
     def to_end(self, request, queryset):
         if len(queryset) != 1:
             return self.message_user(
@@ -416,10 +411,9 @@ class ActivityAdmin(admin.ModelAdmin):
             return self.message_user(request=request,
                                     message='修改成功, 并移除了定时任务!')
         except:
-            return self.message_user(request=request,
-                                    message='修改成功!')
+            return self.message_user(request=request, message='修改成功!')
 
-    @as_action("取消 定时任务")
+    @as_action("取消 定时任务", actions)
     def cancel_scheduler(self, request, queryset):
         success_list = []
         failed_list = []
