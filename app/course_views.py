755f01b9b87a0b73af95dcb451a6dfcc53dd84c1
Haotian's Surface <1357963749@qq.com>
2022-02-23 19:05:22 +0800

    修改title名字

--- app/course_views.py
+++ app/course_views.py
@@ -536,7 +536,7 @@ def viewCourse(request):
     me = utils.get_person_or_org(request.user, user_type)
     course_display = course_to_display(course, me, detail=True)
 
-    bar_display = utils.get_sidebar_and_navbar(request.user, "课程详情")
+    bar_display = utils.get_sidebar_and_navbar(request.user, course.name)
 
     return render(request, "course_info.html", locals())
 
