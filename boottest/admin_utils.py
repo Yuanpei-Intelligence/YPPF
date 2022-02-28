936026809b2eee983bd13a215abaea0c87352f68
panhongtao <59553164+panhongtao07@users.noreply.github.com>
2022-02-28 17:01:57 +0800

    尝试增加后台权限支持

--- boottest/admin_utils.py
+++ boottest/admin_utils.py
@@ -1,12 +1,20 @@
-from functools import wraps
+from typing import Union
+from functools import wraps, update_wrapper
+
 from django.db import transaction
 from django.contrib import messages
 from django.contrib.admin import ModelAdmin
 
 __all__ = [
     'as_display', 'as_action',
-    'need_all_perms',
+    'perms_check', 'need_all_perms',
 ]
+
+
+def _as_perms(permissions):
+    if isinstance(permissions, str):
+        return [permissions]
+    return permissions
 
 
 def as_display(description=None, /, register_to=None, *,
@@ -36,11 +44,12 @@ def as_display(description=None, /, register_to=None, *,
     return actual_decorator
 
 
-def as_action(description=None, /, register_to=None, *,
+def as_action(description=None, /, register_to=None, permissions=None, *,
               superuser=True, single=False, atomic=False, update=False):
     '''
     将函数转化为操作的形式，并试图注册
     检查用户是否有权限执行操作，有权限时捕获错误
+    权限是列表，单个权限可只传入字符串
     关键字参数进行检查或启用必要的环境
     '''
     def actual_decorator(action_function):
@@ -65,6 +74,8 @@ def as_action(description=None, /, register_to=None, *,
                 return self.message_user(request=request,
                                          message=f'操作时发生{type(e)}异常: {e}',
                                          level=messages.ERROR)
+        if permissions is not None:
+            _wrapped_action.allowed_permissions = _as_perms(permissions)
         if description is not None:
             _wrapped_action.short_description = description
         if register_to is not None and _wrapped_action.__name__ not in register_to:
@@ -73,19 +84,40 @@ def as_action(description=None, /, register_to=None, *,
     return actual_decorator
 
 
-def need_all_perms(necessary_perms=None, optional_perms=None):
+def perms_check(necessary_perms: Union[str, list]=None,
+                optional_perms: Union[str, list]=None, *,
+                superuser=False):
+    '''检查函数，必须具有全部必要权限和至少一个可选权限（如果非空），单个权限可为字符串'''
+    def _check_func(self: ModelAdmin, request):
+        if superuser and not request.user.is_admin:
+            return False
+        necessary_checks = (
+            getattr(self, f'has_{perm}_permission')
+            for perm in _as_perms(necessary_perms) or []
+        )
+        if not all(has_perm(request) for has_perm in necessary_checks):
+            return False
+        optional_checks = (
+            getattr(self, f'has_{perm}_permission')
+            for perm in _as_perms(optional_perms) or []
+        )
+        if any(has_perm(request) for has_perm in optional_checks):
+            return True
+        elif optional_checks:
+            return False
+        return True
+    return _check_func
+
+
+def need_all_perms(necessary_perms: Union[str, list]=None,
+                   optional_perms: Union[str, list]=None, *,
+                   superuser=False):
     '''包装器，完全取代被包装函数，通过对象的has_$perm$_permission方法检查'''
     def actual_decorator(check_function):
-        @wraps(check_function)
-        def _check_func(self: ModelAdmin, request, obj=None):
-            for perm in necessary_perms or []:
-                check_func = self.get_attr(f'has_{perm}_permission')
-                if not check_func(request, obj):
-                    return False
-            for perm in optional_perms or []:
-                check_func = self.get_attr(f'has_{perm}_permission')
-                if check_func(request, obj):
-                    return True
-            return True
+        _check_func = perms_check(
+            necessary_perms, optional_perms,
+            superuser=superuser,
+        )
+        update_wrapper(_check_func, check_function)
         return _check_func
     return actual_decorator
