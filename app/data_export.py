b166dc03221ab0022bbda546e383bdcda0bee6a3
Weinan Qian <95567950+SouthwestWindQ@users.noreply.github.com>
2022-07-10 18:02:31 +0800

    Add data masking

--- app/data_export.py
+++ app/data_export.py
@@ -20,7 +20,7 @@ from django.db import transaction
 
 __all__ = [
     # utils
-    'try_output',
+    'try_output', 'data_masking',
     # get data
     'appointment_data',
     # export func
@@ -41,8 +41,18 @@ def try_output(msg: str, output_func=None, html=True):
     else:
         return msg        # output_func为None，返回msg的内容
 
+    
+def data_masking(sid: str) -> str:
+    '''
+    数据脱敏工具函数，根据提供的学生学号给出对应的脱敏结果
+    '''
+    str1 = sid[-3:] + sid[:2] + sid[4]   # 第一步：取学号头2位、第5位和后3位，乱序拼接
+    str2 = str1[2:5] + str1[0] + str1[5] + str1[1]  # 第二步：不同数字以固定规则交换
+    str3 = str((int(str2) + 674831) % 1000000)    # 第三步：加上一个数实现整体平移
+    return "同学" + str3
 
-def appointment_data() -> list:
+
+def appointment_data(require_data_masking: bool) -> list:
     '''
     获取地下室预约的所有数据，返回一个list，其元素为若干记录预约信息的dict
     
@@ -55,23 +65,25 @@ def appointment_data() -> list:
     for appoint in appoint_queryset:
         appointment = {
             "预约房间": appoint.Room.Rid.strip('"') + " " + appoint.Room.Rtitle.strip('"'),
-            "预约人": appoint.major_student.name,
+            "预约人": data_masking(appoint.major_student.Sid) if require_data_masking \
+                         else appoint.major_student.name,
             "参与者": [],
             "开始时间": appoint.Astart.strftime("%Y年%m月%d日 %H:%M"),
             "结束时间": appoint.Afinish.strftime("%Y年%m月%d日 %H:%M"),
             "预约用途": appoint.Ausage,
         }
         for student in appoint.students.all():
-            appointment["参与者"].append(student.name)
+            appointment["参与者"].append(data_masking(student.Sid) if require_data_masking \
+                                      else student.name)
         appointments.append(appointment)
     return appointments
     
 
-def export_underground(filepath, output_func=None, html=False):
+def export_underground(filepath, output_func=None, html=False, require_data_masking=False):
     '''
     将地下室预约的所有数据导出为json文件，输出/返回导出成功信息
     '''
-    appointments = appointment_data()
+    appointments = appointment_data(require_data_masking)
     json_appointments = json.dumps(appointments, ensure_ascii=False, indent=1)
     with open(f"test_data/{filepath}", "w", newline='\n') as jsonfile:
         jsonfile.write(json_appointments)
