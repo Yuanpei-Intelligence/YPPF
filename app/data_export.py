1c847ec24fe1f3abac9a72c49663ca62a53fdbea
Weinan Qian <95567950+SouthwestWindQ@users.noreply.github.com>
2022-07-10 16:28:02 +0800

    data export

---
+++ app/data_export.py
@@ -0,0 +1,89 @@
+from logging.config import listen
+from app.constants import *
+from app.models import (
+    NaturalPerson,
+)
+from Appointment.models import (
+    Appoint,
+)
+from app.utils import random_code_init, get_user_by_name
+
+import os
+import json
+import numpy
+import pandas as pd
+from datetime import datetime
+
+from boottest import local_dict
+from django.contrib.auth.models import User
+from django.shortcuts import render
+from django.db import transaction
+
+__all__ = [
+    # utils
+    'create_user', 'create_person', 'create_org',
+    'create_person_account', 'create_org_account',
+    'try_output',
+    # basic loads
+    'load_stu', 'load_orgtype', 'load_org',
+    # executive functions
+    'load_activity_info', 'load_transfer_info', 'load_notification_info',
+    'load_freshman_info', 'load_help', 'load_course_record', 
+    'load_org_tag', 'load_tags_for_old_org', 'load_feedback_type', 
+    'load_feedback', 'load_feedback_comments', 'load_feedback_data',
+    # views
+    'load_org_view', 'load_activity_info_view', 'load_transfer_info_view',
+    'load_notification_info_view', 'load_stu_view', 'load_freshman_info_view',
+    'load_help_view', 'load_course_record_view', 'load_org_tag_view',
+    'load_tags_for_old_org_view', 'load_feedback_view',
+]
+
+
+def try_output(msg: str, output_func=None, html=True):
+    '''
+    工具函数，尝试用output_func输出msg的内容，如output_func为None则直接返回msg
+    '''
+    if not html:          # 如果不是呈现在html文档，则将<br/>标签换为\n
+        msg = msg.replace('<br/>', '\n')
+
+    if output_func is not None:
+        output_func(msg)  # output_func不为None，直接用output_func输出msg
+        return None
+    else:
+        return msg        # output_func为None，返回msg的内容
+
+
+def appointment_data() -> list:
+    '''
+    获取地下室预约的所有数据，返回一个list，其元素为若干记录预约信息的dict
+    
+    预约信息包含：预约房间、预约人、参与者、开始时间、结束时间、预约用途
+    '''
+    appoint_queryset = Appoint.objects.all()
+    if appoint_queryset.count() == 0:
+        return []
+    appointments = []
+    for appoint in appoint_queryset:
+        appointment = {
+            "预约房间": appoint.Room.Rid.strip('"') + " " + appoint.Room.Rtitle.strip('"'),
+            "预约人": appoint.major_student.name,
+            "参与者": [],
+            "开始时间": appoint.Astart.strftime("%Y年%m月%d日 %H:%M"),
+            "结束时间": appoint.Afinish.strftime("%Y年%m月%d日 %H:%M"),
+            "预约用途": appoint.Ausage,
+        }
+        for student in appoint.students.all():
+            appointment["参与者"].append(student.name)
+        appointments.append(appointment)
+    return appointments
+    
+
+def export_underground(filepath, output_func=None, html=False):
+    '''
+    将地下室预约的所有数据导出为json文件，输出/返回导出成功信息
+    '''
+    appointments = appointment_data()
+    json_appointments = json.dumps(appointments, ensure_ascii=False, indent=1)
+    with open(f"test_data/{filepath}", "w", newline='\n') as jsonfile:
+        jsonfile.write(json_appointments)
+    return try_output("地下室预约信息导出成功！", output_func, html)
