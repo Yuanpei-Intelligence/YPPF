827b45fbb250bcd0a5acdadb2b8a2be16593f953
sunyb-dot <18211195741@126.com>
2022-07-16 14:36:14 +0800

    初步实现load command

--- data_analysis/management/commands/load.py
+++ data_analysis/management/commands/load.py
@@ -1,12 +1,23 @@
-from django.core.management.base import BaseCommand
+from django.core.management.base import BaseCommand, CommandParser
 from data_analysis.management import load_map
 
 class Command(BaseCommand):
-    help = NotImplemented
+    help = 'NotImplemented'
 
-    def add_arguments(self, parser):
+    def add_arguments(self, parser: CommandParser):
         # Positional arguments
-        parser.add_argument('labels', nargs='+', type=str)
+        parser.add_argument(
+            'labels', nargs='+', type=str, help='要导入的模块'
+        )
+        parser.add_argument(
+            '-d', '--dir', type=str,
+            help='测试数据文件所在的文件夹'
+        )
+        parser.add_argument(
+            '-f', '--filename', type=str,
+            help='测试数据文件名'
+        )
+
 
     def handle(self, *args, **options):
         for label in options['labels']:
