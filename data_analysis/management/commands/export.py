9629b073255eec98634dd7576be6ac7a4d7aeaa4
liangrrr <mrliangboqiang@gmail.com>
2022-07-19 19:51:36 +0800

    bug fix

--- data_analysis/management/commands/export.py
+++ data_analysis/management/commands/export.py
@@ -3,50 +3,55 @@ export.py
 
 export命令提供以xlsx格式导出记录的功能
 """
-import os
 
 from data_analysis.management import dump_map
 
+import os
 from datetime import datetime
 import pandas as pd
-from django.core.management.base import BaseCommand, CommandError
-from random import randint
-from hashlib import sha512
+from django.core.management.base import BaseCommand
+from boottest.hasher import MySHA256Hasher
 
 
 def valid_datetime(s: str) -> datetime:
-    """判断输入的日期(str)是否合法，并转化成datetime"""
-    try:
-        return datetime.strptime(s, "%Y-%m-%d")
-    except ValueError:
-        try:
-            return datetime.strptime(s, "%Y-%m-%d-%H:%M")
-        except ValueError:
-            msg = '日期格式错误："{0}"，格式："YY-mm-dd-HH:MM"或"YY-mm-dd"'.format(s)
-            raise ValueError(msg)
+    """判断输入的日期(str)是否合法，并转化成datetime
+
+    Args:
+        s (str): "YY-mm-dd-HH:MM"或"YY-mm-dd"格式日期
+
+    Raises:
+        ValueError: 日期不符合上述格式
+
+    Returns:
+        datetime: 处理后的开始/结束时间
+    """    
+    try: return datetime.strptime(s, "%Y-%m-%d")
+    except: pass
+    try: return datetime.strptime(s, "%Y-%m-%d-%H:%M")
+    except: pass
+    msg = '日期格式错误："{0}"，格式："YY-mm-dd-HH:MM"或"YY-mm-dd"'.format(s)
+    raise ValueError(msg)
 
 
 def filename_modifier(filename: str, taskname: str) -> str:
-    """处理缺省的文件名和没有后缀的文件名"""
+    """处理缺省的文件名和没有后缀的文件名
+
+    Args:
+        filename (str): 待处理的文件名
+        taskname (str): 任务名称，用于寻找默认文件名
+
+    Returns:
+        str: 处理后的文件名
+    """    
     if filename == '':
         # 如果是合并输出，使用'export + 时间戳'作为默认文件名
         if taskname == 'combine':
-            filename = 'export ' + datetime.now().strftime("%H:%M:%S")
+            filename = 'export ' + datetime.now().strftime("%Y-%m-%d")
         else:
             filename = dump_map[taskname][1]
-    if filename[-5:] != '.xlsx' and filename[-4:] != '.xls':
+    if filename.endswith(('.xlsx', '.xls')) is False:
         filename += '.xlsx'
     return filename
-
-
-class HashClass:
-    """本class定义了哈希化的函数，"""
-    def __init__(self, salt: str = ''):
-        self.salt = salt
-
-    def hash_func(self, s: str) -> str:
-        """采用sha512进行哈希化，可修改"""
-        return sha512((s + self.salt).encode('utf-8')).hexdigest()
 
 
 class Command(BaseCommand):
@@ -71,7 +76,7 @@ class Command(BaseCommand):
         # 设置哈希函数
         hash_func = None
         if options['mask'] is True:
-            hash_func = HashClass(options['salt']).hash_func
+            hash_func = MySHA256Hasher(options['salt']).encode
 
         # 检测指定的输出文件名与指定的输出对象数量是否一致，不足则补全
         tasks = options['targets']
