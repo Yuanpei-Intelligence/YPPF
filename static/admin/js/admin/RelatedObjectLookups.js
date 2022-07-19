fc21acdd21b2df5329aff4eb4ba3a8fdfa504245
Zhao Haochen <jtnkc@icloud.com>
2022-07-19 22:21:38 +0800

    修改书院课程数据以及反馈数据的导出函数

--- static/admin/js/admin/RelatedObjectLookups.js
+++ static/admin/js/admin/RelatedObjectLookups.js
@@ -1,17 +1,38 @@
 /*global SelectBox, interpolate*/
 // Handles related-objects functionality: lookup link for raw_id_fields
 // and Add Another links.
-'use strict';
-{
-    const $ = django.jQuery;
+
+(function($) {
+    'use strict';
+
+    // IE doesn't accept periods or dashes in the window name, but the element IDs
+    // we use to generate popup window names may contain them, therefore we map them
+    // to allowed characters in a reversible way so that we can locate the correct
+    // element when the popup window is dismissed.
+    function id_to_windowname(text) {
+        text = text.replace(/\./g, '__dot__');
+        text = text.replace(/\-/g, '__dash__');
+        return text;
+    }
+
+    function windowname_to_id(text) {
+        text = text.replace(/__dot__/g, '.');
+        text = text.replace(/__dash__/g, '-');
+        return text;
+    }
 
     function showAdminPopup(triggeringLink, name_regexp, add_popup) {
-        const name = triggeringLink.id.replace(name_regexp, '');
-        const href = new URL(triggeringLink.href);
+        var name = triggeringLink.id.replace(name_regexp, '');
+        name = id_to_windowname(name);
+        var href = triggeringLink.href;
         if (add_popup) {
-            href.searchParams.set('_popup', 1);
+            if (href.indexOf('?') === -1) {
+                href += '?_popup=1';
+            } else {
+                href += '&_popup=1';
+            }
         }
-        const win = window.open(href, name, 'height=500,width=800,resizable=yes,scrollbars=yes');
+        var win = window.open(href, name, 'height=500,width=800,resizable=yes,scrollbars=yes');
         win.focus();
         return false;
     }
@@ -21,9 +42,9 @@
     }
 
     function dismissRelatedLookupPopup(win, chosenId) {
-        const name = win.name;
-        const elem = document.getElementById(name);
-        if (elem.classList.contains('vManyToManyRawIdAdminField') && elem.value) {
+        var name = windowname_to_id(win.name);
+        var elem = document.getElementById(name);
+        if (elem.className.indexOf('vManyToManyRawIdAdminField') !== -1 && elem.value) {
             elem.value += ',' + chosenId;
         } else {
             document.getElementById(name).value = chosenId;
@@ -36,15 +57,15 @@
     }
 
     function updateRelatedObjectLinks(triggeringLink) {
-        const $this = $(triggeringLink);
-        const siblings = $this.nextAll('.view-related, .change-related, .delete-related');
+        var $this = $(triggeringLink);
+        var siblings = $this.nextAll('.view-related, .change-related, .delete-related');
         if (!siblings.length) {
             return;
         }
-        const value = $this.val();
+        var value = $this.val();
         if (value) {
             siblings.each(function() {
-                const elm = $(this);
+                var elm = $(this);
                 elm.attr('href', elm.attr('data-href-template').replace('__fk__', value));
             });
         } else {
@@ -53,14 +74,14 @@
     }
 
     function dismissAddRelatedObjectPopup(win, newId, newRepr) {
-        const name = win.name;
-        const elem = document.getElementById(name);
+        var name = windowname_to_id(win.name);
+        var elem = document.getElementById(name);
         if (elem) {
-            const elemName = elem.nodeName.toUpperCase();
+            var elemName = elem.nodeName.toUpperCase();
             if (elemName === 'SELECT') {
                 elem.options[elem.options.length] = new Option(newRepr, newId, true, true);
             } else if (elemName === 'INPUT') {
-                if (elem.classList.contains('vManyToManyRawIdAdminField') && elem.value) {
+                if (elem.className.indexOf('vManyToManyRawIdAdminField') !== -1 && elem.value) {
                     elem.value += ',' + newId;
                 } else {
                     elem.value = newId;
@@ -69,8 +90,8 @@
             // Trigger a change event to update related links if required.
             $(elem).trigger('change');
         } else {
-            const toId = name + "_to";
-            const o = new Option(newRepr, newId);
+            var toId = name + "_to";
+            var o = new Option(newRepr, newId);
             SelectBox.add_to_cache(toId, o);
             SelectBox.redisplay(toId);
         }
@@ -78,9 +99,9 @@
     }
 
     function dismissChangeRelatedObjectPopup(win, objId, newRepr, newId) {
-        const id = win.name.replace(/^edit_/, '');
-        const selectsSelector = interpolate('#%s, #%s_from, #%s_to', [id, id, id]);
-        const selects = $(selectsSelector);
+        var id = windowname_to_id(win.name).replace(/^edit_/, '');
+        var selectsSelector = interpolate('#%s, #%s_from, #%s_to', [id, id, id]);
+        var selects = $(selectsSelector);
         selects.find('option').each(function() {
             if (this.value === objId) {
                 this.textContent = newRepr;
@@ -97,9 +118,9 @@
     }
 
     function dismissDeleteRelatedObjectPopup(win, objId) {
-        const id = win.name.replace(/^delete_/, '');
-        const selectsSelector = interpolate('#%s, #%s_from, #%s_to', [id, id, id]);
-        const selects = $(selectsSelector);
+        var id = windowname_to_id(win.name).replace(/^delete_/, '');
+        var selectsSelector = interpolate('#%s, #%s_from, #%s_to', [id, id, id]);
+        var selects = $(selectsSelector);
         selects.find('option').each(function() {
             if (this.value === objId) {
                 $(this).remove();
@@ -107,6 +128,10 @@
         }).trigger('change');
         win.close();
     }
+
+    // Global for testing purposes
+    window.id_to_windowname = id_to_windowname;
+    window.windowname_to_id = windowname_to_id;
 
     window.showRelatedObjectLookupPopup = showRelatedObjectLookupPopup;
     window.dismissRelatedLookupPopup = dismissRelatedLookupPopup;
@@ -128,7 +153,7 @@
         $('body').on('click', '.related-widget-wrapper-link', function(e) {
             e.preventDefault();
             if (this.href) {
-                const event = $.Event('django:show-related', {href: this.href});
+                var event = $.Event('django:show-related', {href: this.href});
                 $(this).trigger(event);
                 if (!event.isDefaultPrevented()) {
                     showRelatedObjectPopup(this);
@@ -136,7 +161,7 @@
             }
         });
         $('body').on('change', '.related-widget-wrapper select', function(e) {
-            const event = $.Event('django:update-related');
+            var event = $.Event('django:update-related');
             $(this).trigger(event);
             if (!event.isDefaultPrevented()) {
                 updateRelatedObjectLinks(this);
@@ -145,11 +170,12 @@
         $('.related-widget-wrapper select').trigger('change');
         $('body').on('click', '.related-lookup', function(e) {
             e.preventDefault();
-            const event = $.Event('django:lookup-related');
+            var event = $.Event('django:lookup-related');
             $(this).trigger(event);
             if (!event.isDefaultPrevented()) {
                 showRelatedObjectLookupPopup(this);
             }
         });
     });
-}
+
+})(django.jQuery);
--- static/admin/js/autocomplete.js
+++ static/admin/js/autocomplete.js
@@ -1,16 +1,12 @@
-'use strict';
-{
-    const $ = django.jQuery;
-    const init = function($element, options) {
-        const settings = $.extend({
+(function($) {
+    'use strict';
+    var init = function($element, options) {
+        var settings = $.extend({
             ajax: {
                 data: function(params) {
                     return {
                         term: params.term,
-                        page: params.page,
-                        app_label: $element.data('app-label'),
-                        model_name: $element.data('model-name'),
-                        field_name: $element.data('field-name')
+                        page: params.page
                     };
                 }
             }
@@ -19,9 +15,9 @@
     };
 
     $.fn.djangoAdminSelect2 = function(options) {
-        const settings = $.extend({}, options);
+        var settings = $.extend({}, options);
         $.each(this, function(i, element) {
-            const $element = $(element);
+            var $element = $(element);
             init($element, settings);
         });
         return this;
@@ -38,4 +34,4 @@
             return $newFormset.find('.admin-autocomplete').djangoAdminSelect2();
         };
     })(this));
-}
+}(django.jQuery));
