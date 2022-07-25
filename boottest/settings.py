41c6f329784d4990243986aecfc67d3505568122
lingbei-meng <2100017808@stu.pku.edu.cn>
2022-07-25 22:54:48 +0800

    代码规范调整

--- boottest/settings.py
+++ boottest/settings.py
@@ -106,8 +106,8 @@ DATABASES = {
         "NAME": base_get_setting('database/NAME'), # local_dict["database"]["NAME"],
         "HOST": base_get_setting('database/HOST', default='127.0.0.1', raise_exception=False),
         "PORT": 3306,
-        "USER": "root",#base_get_setting('database/USER'), # local_dict["database"]["USER"],
-        "PASSWORD": "2100017808",#base_get_setting('database/PASSWORD'), # local_dict["database"]["PASSWORD"],
+        "USER": base_get_setting('database/USER'), # local_dict["database"]["USER"],
+        "PASSWORD": base_get_setting('database/PASSWORD'), # local_dict["database"]["PASSWORD"],
         'OPTIONS': {
             'charset': 'utf8mb4',
         #     "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
