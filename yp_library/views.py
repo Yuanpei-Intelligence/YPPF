6cb7235de37bb3656817dfe5d86b72c8c21af03c
lingbei-meng <2100017808@stu.pku.edu.cn>
2022-07-26 11:21:16 +0800

    修改图标

--- yp_library/views.py
+++ yp_library/views.py
@@ -5,7 +5,7 @@ def welcome(request):
     bar_display = get_sidebar_and_navbar(request.user, "元培书房")
     frontend_dict = {
         "bar_display": bar_display,
-    }                                # 该字典存储提供给前端的信息
+    }                                
     return render(request, "yp_library/welcome.html", frontend_dict)
 
 
@@ -13,7 +13,7 @@ def search(request):
     bar_display = get_sidebar_and_navbar(request.user, "书籍搜索结果")
     frontend_dict = {
         "bar_display": bar_display,
-     }                              # 该字典存储提供给前端的信息
+     }                              
     return render(request, "yp_library/search.html", frontend_dict)
 
 
@@ -21,5 +21,5 @@ def lendInfo(request):
     bar_display = get_sidebar_and_navbar(request.user, "借阅信息")
     frontend_dict = {
         "bar_display": bar_display,
-     }                              # 该字典存储提供给前端的信息
+     }                              
     return render(request, "yp_library/lendinfo.html", frontend_dict)
