14a23094307a6b7541221217ec6297ea06232d5b
MOLYHECI <molyheci@outlook.com>
2022-08-02 09:22:13 +0800

    修改welcome活动一览区域的显示顺序，优先展示非书院课活动

--- app/models.py
+++ app/models.py
@@ -820,8 +820,8 @@ class ActivityManager(models.Manager):
         return select_current(
             self.filter(end__gt=mintime, status=Activity.Status.END))
 
-    def get_recent_activity(self):
-        # 开始时间在前后一周内，除了取消和审核中的活动。按时间逆序排序
+    def get_recent_normal_activity(self):
+        # 开始时间在前后一周内，除了取消和审核中的非书院课活动。按时间逆序排序
         nowtime = datetime.now()
         mintime = nowtime - timedelta(days=7)
         maxtime = nowtime + timedelta(days=7)
@@ -833,11 +833,34 @@ class ActivityManager(models.Manager):
                 Activity.Status.WAITING,
                 Activity.Status.PROGRESSING,
                 Activity.Status.END
+            ],
+            category__in=[
+                Activity.ActivityCategory.NORMAL,
+                Activity.ActivityCategory.ELECTION
+            ]
+        )).order_by("-start")
+        
+    def get_recent_course_activity(self):
+            # 开始时间在前后一周内，除了取消和审核中的书院课活动。按时间逆序排序
+        nowtime = datetime.now()
+        mintime = nowtime - timedelta(days=7)
+        maxtime = nowtime + timedelta(days=7)
+        return select_current(self.filter(
+            start__gt=mintime,
+            start__lt=maxtime,
+            status__in=[
+                Activity.Status.APPLYING,
+                Activity.Status.WAITING,
+                Activity.Status.PROGRESSING,
+                Activity.Status.END
+            ],
+            category__in=[
+                Activity.ActivityCategory.COURSE
             ]
         )).order_by("-start")
 
-    def get_newlyreleased_activity(self):
-        # 最新一周内发布的活动，按发布的时间逆序
+    def get_newlyreleased_normal_activity(self):
+        # 最新一周内发布的非书院课活动，按发布的时间逆序
         nowtime = datetime.now()
         return select_current(self.filter(
             publish_time__gt=nowtime - timedelta(days=7),
@@ -845,6 +868,25 @@ class ActivityManager(models.Manager):
                 Activity.Status.APPLYING,
                 Activity.Status.WAITING,
                 Activity.Status.PROGRESSING
+            ],
+            category__in=[
+                Activity.ActivityCategory.NORMAL,
+                Activity.ActivityCategory.ELECTION
+            ]
+        )).order_by("-publish_time")
+    
+    def get_newlyreleased_course_activity(self):
+        # 最新一周内发布的书院课活动，按发布的时间逆序
+        nowtime = datetime.now()
+        return select_current(self.filter(
+            publish_time__gt=nowtime - timedelta(days=7),
+            status__in=[
+                Activity.Status.APPLYING,
+                Activity.Status.WAITING,
+                Activity.Status.PROGRESSING
+            ],
+            category__in=[
+                Activity.ActivityCategory.COURSE
             ]
         )).order_by("-publish_time")
 
