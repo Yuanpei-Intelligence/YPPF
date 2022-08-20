3a9acc1739a82f8296ece04396332909d1f8acb5
panhongtao <59553164+panhongtao07@users.noreply.github.com>
2022-08-20 14:19:51 +0800

    提供可查看的用户后台

--- generic/admin.py
+++ generic/admin.py
@@ -2,4 +2,12 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from generic.models import User
 
-admin.site.register(User, UserAdmin)
+
+@admin.register(User)
+class MyUserAdmin(UserAdmin):
+    list_display = ['id', 'username', 'credit', 'utype', 'is_staff', 'is_superuser']
+    list_editable = ['credit']
+    list_filter = ['utype', 'is_superuser', 'is_staff', 'groups', 'is_active']
+    fieldsets = UserAdmin.fieldsets + (
+        ('自定义信息', {'fields': ['credit', 'YQpoint', 'utype']}),
+    )
