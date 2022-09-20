dc232220a68848c16e16d72da30d89ca69d11b2d
panhongtao <59553164+panhongtao07@users.noreply.github.com>
2022-09-21 00:35:20 +0800

    增加HttpRequest类型提示

--- generic/http/dependency.py
+++ generic/http/dependency.py
@@ -1,3 +1,4 @@
-from django.http import HttpResponse, HttpRequest, JsonResponse
+from .rewrite_httprequest_user import HttpRequest
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import redirect
 from django.urls import reverse
