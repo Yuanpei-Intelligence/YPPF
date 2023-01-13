531735f110e0947ac863db856089ee8d2a51182d
pkuGenuine <1800017716@pku.edu.cn>
2023-01-13 15:44:14 +0800

    Add script to run the project more easily

--- boot/settings.py
+++ boot/settings.py
@@ -1,11 +1,11 @@
 import os
 import logging
 
+from boot import base_get_setting
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
-from boot import base_get_setting
 
 # LOGIN_URL，未登录时重定向到的 URL
 LOGIN_URL = base_get_setting('url/login_url')
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
-    #'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
@@ -75,7 +75,8 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = "boot.wsgi.application"
-AUTHENTICATION_BACKENDS = ["django.contrib.auth.backends.AllowAllUsersModelBackend"]
+AUTHENTICATION_BACKENDS = [
+    "django.contrib.auth.backends.AllowAllUsersModelBackend"]
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
@@ -95,14 +96,17 @@ DATABASES = {
     # create database underground charset='utf8mb4';
     "default": {
         "ENGINE": "django.db.backends.mysql",
-        "NAME": base_get_setting('database/NAME'), # local_dict["database"]["NAME"],
+        # local_dict["database"]["NAME"],
+        "NAME": base_get_setting('database/NAME'),
         "HOST": base_get_setting('database/HOST', default='127.0.0.1', raise_exception=False),
         "PORT": 3306,
-        "USER": base_get_setting('database/USER'), # local_dict["database"]["USER"],
-        "PASSWORD": base_get_setting('database/PASSWORD'), # local_dict["database"]["PASSWORD"],
+        # local_dict["database"]["USER"],
+        "USER": base_get_setting('database/USER'),
+        # local_dict["database"]["PASSWORD"],
+        "PASSWORD": base_get_setting('database/PASSWORD'),
         'OPTIONS': {
             'charset': 'utf8mb4',
-        #     "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
+            #     "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
         },
         'TEST': {
             'CHARSET': 'utf8',
@@ -118,9 +122,9 @@ AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
-    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",},
-    {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",},
-    {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",},
+    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator", },
+    {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator", },
+    {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator", },
 ]
 
 
@@ -166,7 +170,7 @@ USE_TZ = False
 MY_STATIC_DIR = os.getenv("YPPF_STATIC_DIR", BASE_DIR)
 MY_TMP_DIR = os.getenv("YPPF_TMP_DIR", BASE_DIR)
 STATIC_URL = "/static/"
-STATICFILES_DIRS = (os.path.join(MY_STATIC_DIR, "static"),)
+STATIC_ROOT = os.path.join(MY_STATIC_DIR, "static")
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(MY_STATIC_DIR, "media/")
@@ -183,7 +187,8 @@ MY_INNER_PORT = os.getenv("YPPF_INNER_PORT", 80)
 
 # LOG
 MY_LOG_DIR = os.getenv("YPPF_LOG_DIR", BASE_DIR)
-MY_LOG_LEVEL = logging.DEBUG if os.getenv("YPPF_LOG_DEBUG", "") else logging.INFO
+MY_LOG_LEVEL = logging.DEBUG if os.getenv(
+    "YPPF_LOG_DEBUG", "") else logging.INFO
 MY_SCHEDULER_LOG = os.getenv("YPPF_SCHEDULER_LOG_FILE", "scheduler.log")
 
 if MY_ENV in ["PRODUCT", "TEST"]:
