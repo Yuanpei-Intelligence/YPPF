bd9b01bd2d0f4de82c80d56995366f52b7f55320
panhongtao <59553164+panhongtao07@users.noreply.github.com>
2023-02-18 00:20:12 +0800

    Update rewrite_httprequest_user.pyi for QueryDict

--- utils/http/rewrite_httprequest_user.pyi
+++ utils/http/rewrite_httprequest_user.pyi
@@ -1,11 +1,18 @@
 from django.http import HttpRequest as _HttpRequest
+from django.http.request import QueryDict as _QueryDict
 from django.contrib.auth.models import AnonymousUser
 from generic.models import User
+
+class QueryDict(_QueryDict):
+    # Actually str | []
+    def __getitem__(self, key: str) -> str: ...
 
 class HttpRequest(_HttpRequest):
     '''An unauthenticated HTTP request.'''
     user: 'User | AnonymousUser'
     method: str
+    GET: QueryDict
+    POST: QueryDict
 
 class UserRequest(HttpRequest):
     '''An authenticated HTTP request'''
