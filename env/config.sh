b03a31eb6649d79831e9b2c207f444965b3d31d1
panhongtao <59553164+panhongtao07@users.noreply.github.com>
2023-02-21 14:40:19 +0800

    移除废弃设置和local_json
    boot初始化设置被移除

--- env/config.sh
+++ env/config.sh
@@ -1,12 +1,6 @@
 #!/usr/bin/env bash
 
 DIR=$( dirname -- "${BASH_SOURCE[0]}" )
-cat $DIR/../local_json_template.json | \
-    sed 's/\$DATABASE\$/yppf/g' | \
-    sed 's/\$USER\$/root/g' | \
-    sed 's/\$PASSWORD\$/secret/g' | \
-    sed 's/\$HOST\$/mysql/g' | \
-    sed 's/\"use_scheduler\": false/\"use_scheduler\": true/g' > $DIR/../local_json.json
 
 cat $DIR/../config_template.json | \
     sed 's/\$DATABASE\$/yppf/g' | \
