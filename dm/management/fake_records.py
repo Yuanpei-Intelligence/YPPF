117e7b6647c56411f5d43aaf857f93497e88e732
sanshisan <1059978460@qq.com>
2023-03-02 12:27:56 +0800

    一名老师，一名学生，两个小组，学生管理小组

--- dm/management/fake_records.py
+++ dm/management/fake_records.py
@@ -3,10 +3,13 @@ Generate fake records. Only used in dev & test.
 """
 
 from django.conf import settings
+
 from generic.models import User
 from app.models import *
 
 USER_NAME = "2000000000"
+TEACHER_NAME = "1800017710"
+
 ORGANIZATION_USER_NAME = ['huihuaban', 'wudaoban']
 
 # TODO: Change Settings
@@ -34,7 +37,7 @@ def create_np():
     # TODO: Modify it
     sid = username = USER_NAME
     password = username
-    name = "小明"
+    name = "王小明"
     gender = NaturalPerson.Gender.MALE
     stu_major = "元培计划（待定）"
     stu_grade = "20" + sid[:2]
@@ -42,13 +45,9 @@ def create_np():
     email = sid + "@stu.pku.edu.cn"
     visit_times = 100
     tel = None
-    biography = '各位考官好，今天能够站在这里参加面试，有机会向各位考官请教和学习，' \
-                '我感到非常的荣幸。希 望通过这次面试能够把自己展示给大家，希望大家记住我。' \
-                '我叫xxx，今年xx岁。汉族，法学本科。我 平时喜欢看书和上网浏览信息。我的性格比较开朗，' \
-                '随和。能关系周围的任何事，和亲人朋友能够和睦 相处，并且对生活充满了信心。我以前在检察院实习过，' \
-                '所以有一定的实践经验。在外地求学的四年 中，我养成了坚强的性格，这种性格使我克服了学习和生活中的一些困难，' \
-                '积极进去。成为一名法律工 作者是我多年以来的强烈愿望。'
+    biography = '我是一名学生'
     first_time_login = False
+    identity = NaturalPerson.Identity.STUDENT
 
     user, created = User.objects.get_or_create(username=username)
     user.set_password(password)
@@ -68,8 +67,39 @@ def create_np():
             telephone=tel,
             visit_times=visit_times,
             biography=biography,
+            identity=identity,
         )
         stu.save()
+
+    sid = username = TEACHER_NAME
+    password = username
+    name = "张老师"
+    gender = NaturalPerson.Gender.MALE
+    email = sid + "@stu.pku.edu.cn"
+    visit_times = 100
+    tel = None
+    biography = '我是一名老师'
+    first_time_login = False
+    identity = NaturalPerson.Identity.TEACHER
+
+    user, created = User.objects.get_or_create(username=username)
+    user.set_password(password)
+    user.first_time_login = first_time_login
+    user.utype = User.Type.PERSON
+    user.save()
+    if created:
+        tea = NaturalPerson.objects.create(
+            person_id=user,
+            stu_id_dbonly=sid,
+            name=name,
+            gender=gender,
+            email=email,
+            telephone=tel,
+            visit_times=visit_times,
+            biography=biography,
+            identity=identity,
+        )
+        tea.save()
 
 
 def create_org_type():
@@ -109,8 +139,12 @@ def create_org_tag():
 
 
 def create_org():
+    first_time_login = False
+
     user, created = User.objects.get_or_create(username=ORGANIZATION_USER_NAME[0])
     user.utype = User.Type.ORG
+    user.first_time_login = first_time_login
+    user.set_password(ORGANIZATION_USER_NAME[0])
     oname = '绘画班'
     otype = OrganizationType.objects.get(otype_id=1)
     tags = OrganizationTag.objects.get(name='兴趣')
@@ -127,6 +161,7 @@ def create_org():
 
     user_2, created_2 = User.objects.get_or_create(username=ORGANIZATION_USER_NAME[1])
     user_2.utype = User.Type.ORG
+    user_2.first_time_login = first_time_login
     oname_2 = '舞蹈班'
     otype_2 = OrganizationType.objects.get(otype_id=1)
     tags_2 = OrganizationTag.objects.get(name='兴趣')
