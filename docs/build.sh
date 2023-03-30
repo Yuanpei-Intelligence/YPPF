677457d2d6bd39d434d27633adab96b07c298972
panhongtao <59553164+panhongtao07@users.noreply.github.com>
2023-03-30 19:08:34 +0800

    使用markdown文档

--- docs/build.sh
+++ docs/build.sh
@@ -5,7 +5,7 @@ cd $( dirname -- "${BASH_SOURCE[0]}" )
 # 如果安装本包，则sphinx会尝试先从此处导入
 pip3 uninstall importlib-metadata -y
 
-pip3 install sphinx python-docs-theme
+pip3 install sphinx python-docs-theme myst-parser
 
 # pip3 freeze
 
