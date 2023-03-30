0117307c4f1cafb364bc237a31adac5554ae543a
panhongtao <59553164+panhongtao07@users.noreply.github.com>
2023-03-30 17:16:24 +0800

    修复sphinx兼容问题

--- docs/build.sh
+++ docs/build.sh
@@ -2,14 +2,13 @@
 
 cd $( dirname -- "${BASH_SOURCE[0]}" )
 
-pip3 install sphinx==4.3 python-docs-theme
+# 如果安装本包，则sphinx会尝试先从此处导入
+pip3 uninstall importlib-metadata -y
+
+pip3 install sphinx python-docs-theme
 
 # pip3 freeze
 
 sphinx-apidoc -e -d 2 -o source/yppf ../
 
-make html
-
-cd build/
-
-ls
+make html && cd build/ && ls
