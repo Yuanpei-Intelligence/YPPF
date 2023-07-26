d98d85194802520fa97035637bfd43df9964b159
susu <drizzle_0505@outlook.com>
2023-07-26 22:12:20 +0800

    改了models一些内容

--- dormitory/models.py
+++ dormitory/models.py
@@ -14,82 +14,80 @@ from generic.models import User
 
 # 调查问卷
 class Survey(models.Model):
-    title = models.CharField(max_length=50) # 标题
-    description = models.TextField() # 描述
-    creator = models.ForeignKey(User, on_delete=models.CASCADE, related_name='creator_survey') # 创建人
-    date = models.DateTimeField(auto_now_add=True, verbose_name='创建时间') # 创建时间
-    is_published = models.BooleanField(default=False) # 是否发布
+    title = models.CharField(verbose_name="问卷标题", max_length=50)
+    description = models.TextField(verbose_name="问卷描述")
+    creator = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="创建人")
+    date = models.DateTimeField(verbose_name="创建时间", auto_now_add=True)
+    is_published = models.BooleanField(verbose_name="发布中", default=False)
+    start_time = models.DateTimeField(verbose_name="起始时间")
+    end_time = models.DateTimeField(verbose_name="截止时间")
 
-    def __str__(self):
-        return f'[问卷] 标题:{self.title};创建人:{self.creator.username};创建时间:{self.date}'
+    # def __str__(self):
+        # return f'[问卷] 标题:{self.title};创建人:{self.creator.username};创建时间:{self.date}'
     
-    class Meta:
-        ordering = ['-date']
+    # class Meta:
+        # ordering = ['-date']
 
 
 # 答卷
 class AnswerSheet(models.Model):
-    survey = models.ForeignKey(Survey, on_delete=models.CASCADE, related_name='survey_answersheet') # 对应问卷
-    creator = models.ForeignKey(User, on_delete=models.CASCADE, related_name='creator_answersheet') # 答卷人
-    date = models.DateTimeField(auto_now_add=True, verbose_name='填写时间') # 填写时间
+    survey = models.ForeignKey(Survey, on_delete=models.CASCADE, verbose_name="对应问卷")
+    creator = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="答卷人")
+    date = models.DateTimeField(verbose_name="填写时间", auto_now_add=True)
 
-    def __str__(self):
-        return f'[答卷] 标题:{self.survey.title};答卷人:{self.creator.username};填写时间:{self.date}'
+    # def __str__(self):
+        # return f'[答卷] 标题:{self.survey.title};答卷人:{self.creator.username};填写时间:{self.date}'
 
-    class Meta:
-        ordering = ['-date']
+    # class Meta:
+        # ordering = ['-date']
 
 
 # 问题
 class Question(models.Model):
-    QTYPE = (
-        ('Text', '填空'), 
-        ('Single', '单选'), 
-        ('Multi', '多选'), 
-        ('Ranking', '排序'), 
-    )
+    TEXT = "Te"
+    SINGLE = "Si"
+    MULTI = "Mu"
+    RANKING = "Ra"
 
-    survey = models.ForeignKey(Survey, on_delete=models.CASCADE, related_name='survey_question') # 对应问卷
-    id = models.IntegerField() # 题目序号
-    text = models.CharField(max_length=50) # 题干
-    description = models.TextField() # 题目描述
-    type = models.CharField(max_length=10, choices=QTYPE) # 问题类型
+    QTYPE = [
+        (TEXT, '填空'), 
+        (SINGLE, '单选'), 
+        (MULTI, '多选'), 
+        (RANKING, '排序'), 
+    ]
 
-    def __str__(self):
-        return f'[问题] 题干:{self.text};序号:{self.id};类型:{self.type}'
+    survey = models.ForeignKey(Survey, on_delete=models.CASCADE, verbose_name="所属问卷")
+    order = models.IntegerField(verbose_name="题目序号")
+    text = models.CharField(verbose_name="题干", max_length=50)
+    description = models.TextField(verbose_name="题目描述")
+    type = models.CharField(verbose_name="问题类型", max_length=10, choices=QTYPE)
+
+    # def __str__(self):
+        # return f'[问题] 题干:{self.text};序号:{self.id};类型:{self.type}'
     
-    class Meta:
-        ordering = ['id']
+    # class Meta:
+        # ordering = ['id']
 
 # 选项
 class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='question_choice') # 对应问题
-    id = models.IntegerField() # 选项序号
-    text = models.TextField() # 选项内容
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='choices', verbose_name="所属问题")
+    order = models.IntegerField(verbose_name="选项序号")
+    text = models.TextField(verbose_name="选项内容")
 
-    def __str__(self):
-        return f'[选项] 内容:{self.text};序号:{self.id}'
+    # def __str__(self):
+        # return f'[选项] 内容:{self.text};序号:{self.id}'
     
-    class Meta:
-        ordering = ['id']
+    # class Meta:
+        # ordering = ['id']
 
 
 # 回答，按字符串形式储存，与user&question建立连接
 class AnswerText(models.Model):
-    survey = models.ForeignKey(Survey, on_delete=models.CASCADE, related_name='survey_answertext') # 对应问卷
-    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='question_answertext') # 对应问题
-    answer = models.TextField() # 答案内容
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, verbose_name="对应问题")
+    answer = models.TextField(verbose_name="答案内容")
 
-    def __str__(self):
-        return f'[答案] 问卷:{self.survey.title};问题:{self.question.text};答案:{self.answer}'
-    
-    class Meta:
-        ordering = ['question__id']
-
-
-# 问卷的统计信息
-class SurveyStatistics(models.Model):
-    pass
+    # class Meta:
+        # ordering = ['question__id']
 
 
 # 补充事项
