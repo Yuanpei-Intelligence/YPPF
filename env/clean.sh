8efe2407a0fe5866e8c9d52896a98354a99bc1d7
pkugenuine <pkugenuine@gmail.com>
2023-07-28 20:57:42 +0800

    Remove old env folder
    Change test workflow file accordingly

--- env/clean.sh
+++
@@ -1,7 +0,0 @@
-read -p "Clean old database and containers? (input y to continue) > " confirm
-if [ "$confirm" = "y" ]; then
-    docker compose run --rm yppf bash env/remove_migrations.sh
-    docker compose down -v
-else
-    exit 1
-fi
--- scripts/default_config.sh
+++ scripts/default_config.sh
@@ -6,4 +6,5 @@ cat $DIR/../config_template.json | \
     sed 's/\$DATABASE\$/yppf/g' | \
     sed 's/\$USER\$/root/g' | \
     sed 's/\$PASSWORD\$/secret/g' | \
+    sed 's/\"\$SCHEDULER_RPC_PORT\$\"/6666/g' | \
     sed 's/\$HOST\$/mysql/g' > $DIR/../config.json
