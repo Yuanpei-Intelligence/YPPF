56fb344406b84b9b089ab1cc817a2e348e1b73db
susu <drizzle_0505@outlook.com>
2023-08-06 19:34:12 +0800

    完善权限

--- questionnaire/apps.py
+++ questionnaire/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class QuestionnaireConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "questionnaire"
+    verbose_name = "3. 问卷系统"
--- questionnaire/models.py
+++ questionnaire/models.py
@@ -1,8 +1,18 @@
 from django.db import models
-from boot.settings import AUTH_USER_MODEL
+
+from generic.models import User
+
 from utils.models.choice import choice
+from utils.models.descriptor import debug_only
 
 
+__all__ = [
+    'Survey', 
+    'AnswerSheet', 
+    'Question', 
+    'Choice', 
+    'AnswerText'
+]
 
 """
 顶级模型:问卷和答卷 需要有创建人/答卷人、创建日期
@@ -14,41 +24,50 @@ from utils.models.choice import choice
 """
 
 
-# 调查问卷
 class Survey(models.Model):
+    '''
+    调查问卷
+    '''
     class Status(models.IntegerChoices):
-        REVIEWING = (0, "审核中")
-        PUBLISHED = (1, "发布中")
-        ENDED = (2, "已结束")
+        REVIEWING = choice(0, "审核中")
+        PUBLISHED = choice(1, "发布中")
+        ENDED = choice(2, "已结束")
 
-    title = models.CharField("问卷标题", max_length=50, unique=True, blank=False, null=False)
+    title = models.CharField("问卷标题", max_length=50, blank=False, null=False)
     description = models.TextField("问卷描述", blank=True)
-    creator = models.ForeignKey(AUTH_USER_MODEL, on_delete=models.CASCADE, related_name = 'surveys', verbose_name="创建人")
+    creator = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="创建人")
     status = models.SmallIntegerField("问卷状态", choices=Status.choices, default=Status.REVIEWING)
-    start_time = models.DateTimeField("起始时间", blank=True)
-    end_time = models.DateTimeField("截止时间", blank=True)
+    start_time = models.DateTimeField("起始时间")
+    end_time = models.DateTimeField("截止时间")
     time = models.DateTimeField("创建时间", auto_now_add=True)
 
+    @debug_only
     def __str__(self):
         return self.title
 
 
-# 答卷
 class AnswerSheet(models.Model):
+    '''
+    答卷
+    '''
     survey = models.ForeignKey(Survey, on_delete=models.CASCADE, verbose_name="对应问卷")
-    creator = models.ForeignKey(AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name="答卷人")
+    creator = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="答卷人")
     create_time = models.DateTimeField("填写时间", auto_now_add=True)
+    # submitted = models.BooleanField("已提交")
 
+    @debug_only
     def __str__(self):
         return self.survey.title + " - " + self.creator.username + "的答卷"
 
 
-# 问题
 class Question(models.Model):
+    '''
+    问题
+    '''
     class Type(models.TextChoices):
         TEXT = choice("TEXT", "填空题")
         SINGLE = choice("SINGLE", "单选题")
-        MULTI = choice("MULTI", "多选题")
+        MULTIPLE = choice("MULTIPLE", "多选题")
         RANKING = choice("RANKING", "排序题")
 
     survey = models.ForeignKey(Survey, on_delete=models.CASCADE, verbose_name="所属问卷")
@@ -58,8 +77,9 @@ class Question(models.Model):
     type = models.CharField("问题类型", max_length=10, choices=Type.choices, default=Type.SINGLE)
 
     def have_choice(self):
-        return self.type in ["SINGLE", "MULT", "RANKING"]
+        return self.type in ["SINGLE", "MULTIPLE", "RANKING"]
 
+    @debug_only
     def __str__(self):
         return self.topic
 
@@ -70,12 +90,15 @@ class Choice(models.Model):
     order = models.IntegerField("选项序号")
     text = models.TextField("选项内容")
 
+    @debug_only
     def __str__(self):
         return self.text
 
 
-# 回答，按字符串形式储存，与user&question建立连接
 class AnswerText(models.Model):
+    '''
+    回答，按字符串形式储存，与user&question建立连接
+    '''
     question = models.ForeignKey(Question, on_delete=models.CASCADE, verbose_name="对应问题")
     answersheet = models.ForeignKey(AnswerSheet, on_delete=models.CASCADE, verbose_name="所属答卷")
     body = models.TextField("答案内容")
