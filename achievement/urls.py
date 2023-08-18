bd63352c940284abc8898cea27f005d341504184
vassagoP <jtnkc@icloud.com>
2023-08-18 22:06:40 +0800

    remove redundant dir

--- achievement/urls.py
+++ achievement/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('achievement/', views.view_achievements, name='view_achievements'),
+    path('', views.view_achievements, name='view_achievements'),
 ]
