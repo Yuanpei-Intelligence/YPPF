20e32927d25c29fad247d95fc0ec67e8adb2733f
panhongtao <59553164+panhongtao07@users.noreply.github.com>
2023-08-24 23:45:57 +0800

    自述：配置文件

--- scripts/default_config.sh
+++ scripts/default_config.sh
@@ -1,10 +1,27 @@
 #!/usr/bin/env bash
 
-DIR=$( dirname -- "${BASH_SOURCE[0]}" )
+DIR=$(dirname "${BASH_SOURCE[0]}")
+BASE=$(cd "$DIR"/.. && pwd)
 
-cat $DIR/../config_template.json | \
-    sed 's/\$DATABASE\$/yppf/g' | \
-    sed 's/\$USER\$/root/g' | \
-    sed 's/\$PASSWORD\$/secret/g' | \
-    sed 's/\"\$SCHEDULER_RPC_PORT\$\"/6666/g' | \
-    sed 's/\$HOST\$/mysql/g' > $DIR/../config.json
+# 如果已经存在 config.json 文件，则判断是否需要覆盖
+if [ -f $BASE/config.json ]
+then
+    read -p "当前目录下已存在 config.json 文件，是否覆盖？[y/N] " -r
+    if [[ $REPLY =~ ^[Yy]$ ]]
+    then
+        echo "覆盖 config.json 文件。"
+    else
+        echo "取消覆盖。"
+        exit 0
+    fi
+fi
+
+# 读取并复制模板文件
+cp $BASE/config_template.json $BASE/config.json
+
+# 替换默认设置
+sed -i 's/\$DATABASE\$/yppf/g' $BASE/config.json
+sed -i 's/\$USER\$/root/g' $BASE/config.json
+sed -i 's/\$PASSWORD\$/secret/g' $BASE/config.json
+sed -i 's/\"\$SCHEDULER_RPC_PORT\$\"/6666/g' $BASE/config.json
+sed -i 's/\$HOST\$/mysql/g' $BASE/config.json
