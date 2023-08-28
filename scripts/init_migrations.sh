3d5534dbba6fa82124c05b58a901888bf2d629cb
panhongtao <59553164+panhongtao07@users.noreply.github.com>
2023-08-29 01:25:40 +0800

    保留默认迁移文件
    修改迁移脚本

--- scripts/init_migrations.sh
+++ scripts/init_migrations.sh
@@ -1,3 +1,2 @@
-python3 manage.py makemigrations generic app record Appointment yp_library \
-    questionnaire dormitory feedback achievement semester
+python3 manage.py makemigrations
 python3 manage.py migrate
