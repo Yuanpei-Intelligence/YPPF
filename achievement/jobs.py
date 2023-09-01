9c5e92bb5de1d1de168a03d7e3cbf3c01adba6e7
pkugenuine <pkugenuine@gmail.com>
2023-09-01 11:57:11 +0000

    Fix

--- achievement/jobs.py
+++ achievement/jobs.py
@@ -2,7 +2,7 @@ from datetime import datetime, time
 
 from achievement.api import bulk_add_achievement_record
 from achievement.models import Achievement
-from achievement.utils import calculate_enrollment_years
+from achievement.utils import get_students_by_grade
 from scheduler.adder import ScheduleAdder
 from scheduler.periodic import periodical
 from semester.api import next_semester
@@ -12,20 +12,18 @@ from semester.api import next_semester
 def start_fall_semester():
     '''在每年9月1日读取秋季开学日期'''
 
-    ScheduleAdder(init_new_year, run_time=datetime.combine(next_semester().start_date, time.min))()
+    ScheduleAdder(init_new_year, run_time=datetime.combine(
+        next_semester().start_date, time.min))()
+
+
+def new_year_achievements():
+    '''触发 元气人生-开启大学第二、三、四年'''
+    for (i, j) in zip(range(2, 7), ['二', '三', '四', '五', '六']):
+        achievement = Achievement.objects.get(name=f'开启大学生活第{j}年')
+        user_list = get_students_by_grade(i)
+        bulk_add_achievement_record(
+            user_list=user_list, achievement=achievement)
 
 
 def init_new_year():
-    '''触发 元气人生-开启大学第二、三、四年'''
-
-    achievement_2 = Achievement.objects.get(name="开启大学生活第二年")
-    user_list_2 = calculate_enrollment_years(2)
-    bulk_add_achievement_record(user_list=user_list_2, achievement=achievement_2)
-
-    achievement_3 = Achievement.objects.get(name="开启大学生活第三年")
-    user_list_3 = calculate_enrollment_years(3)
-    bulk_add_achievement_record(user_list=user_list_3, achievement=achievement_3)
-
-    achievement_4 = Achievement.objects.get(name="开启大学生活第四年")
-    user_list_4 = calculate_enrollment_years(4)
-    bulk_add_achievement_record(user_list=user_list_4, achievement=achievement_4)
+    new_year_achievements()
--- achievement/utils.py
+++ achievement/utils.py
@@ -1,20 +1,16 @@
-from datetime import datetime
-
 from generic.models import User
 from semester.api import current_semester
 
-def calculate_enrollment_years(goal: int):
+
+def get_students_by_grade(grade: int) -> list[User]:
     '''
     传入目标入学年份数，返回满足的、未毕业的学生User列表。
     示例：今年是2023年，我希望返回入学第二年的user，即查找username前两位为22的user
     仅限秋季学期开始后使用。
     '''
 
-    semester_date = current_semester().start_date
-    if semester_date.month == 9:
-        goal_year = semester_date.year - 2000 - goal + 1
-    elif semester_date.month == 2:
-        goal_year = semester_date.year - 2000 - goal
-    user = User.objects.filter(utype=User.Type.STUDENT, active=True, username__startswith=str(goal_year))
-
+    semester_year = current_semester().year
+    goal_year = semester_year - 2000 - grade + 1
+    user = User.objects.filter(
+        utype=User.Type.STUDENT, active=True, username__startswith=str(goal_year))
     return list(user)
