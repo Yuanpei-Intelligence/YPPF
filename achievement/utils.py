12a67f99051fa2cc03ca3debb5b4a2ad853d0302
vassagoP <jtnkc@icloud.com>
2023-09-04 20:18:10 +0800

    bug fix

--- achievement/utils.py
+++ achievement/utils.py
@@ -15,23 +15,20 @@ def stuinfo_set_achievement(user):
 
     unlocked_achievements = AchievementUnlock.objects.filter(user=user)
     achievement_types = AchievementType.objects.all().order_by('id')
-        # unlocked_personal_stat: 字典，从achievementType到该用户achievementUnlock的数量
-        # all_achievement_stat: 字典，从achievementType到该类型下Achievement的总数量
-    unlocked_personal_stat = {}
-    all_achievement_stat = {}
+    display_tuple = []  # (achievement_type, unlocked num, all num)
     for a_t in achievement_types:
-        all_achievement_stat[a_t] = Achievement.objects.filter(
+        all_achievement_stat = Achievement.objects.filter(
                 achievement_type=a_t).count()
         achievement_a_t = Achievement.objects.filter(achievement_type=a_t)
-        unlocked_personal_stat[a_t] = unlocked_achievements.filter(
+        unlocked_personal_stat = unlocked_achievements.filter(
                 achievement__in=achievement_a_t).count()
+        display_tuple.append((a_t, unlocked_personal_stat, all_achievement_stat))
         # print("unlocked_personal_stat", unlocked_personal_stat)
         # print("all_achievement_stat", all_achievement_stat)
         
         # works, but ugly, need to be improved in the future
-    achievement_types_0 = achievement_types[:3]
-    achievement_types_1 = achievement_types[3:6]
-    achievement_types_2 = achievement_types[6:9]
-	# print("achievement_types_0", achievement_types_0)
+    achievement_types_0 = display_tuple[:3]
+    achievement_types_1 = display_tuple[3:6]
+    achievement_types_2 = display_tuple[6:9]
     return invisible_achievements, visible_achievements, achievement_types_0, achievement_types_1, achievement_types_2
 
