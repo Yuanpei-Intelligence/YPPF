975c9c024c3874a0590823ababd959c8a4df39b6
zrbpkuyp <2100017798@stu.pku.edu.cn>
2023-09-08 10:21:33 +0800

    排版

--- achievement/jobs.py
+++ achievement/jobs.py
@@ -23,19 +23,25 @@ def unlock_credit_achievements():
     DAYS_LIMIT = 21
     # 如果当前日期位于学期中间，进行'当月没有扣除信用分'成就的触发
     if semester.start_date <= today < semester.end_date: # 注意end_date是指放假开始的当天
-        if (today - semester.start_date).days >= DAYS_LIMIT: # 上月最后一天距离学期初的天数超过阈值
-            unlock_credit_achievements_by_name(last_month_firstday, last_month_lastday, '当月没有扣除信用分')
+        # 上月在学期内的天数超过阈值
+        if (today - semester.start_date).days >= DAYS_LIMIT: 
+            unlock_credit_achievements_by_name(
+                last_month_firstday, last_month_lastday, '当月没有扣除信用分')
     # 如果当前日期位于学期结束后
     elif today >= semester.end_date:
         # 进行'一学期没有扣除信用分'成就的触发
-        unlock_credit_achievements_by_name(semester.start_date, semester.end_date-timedelta(days=1), '一学期没有扣除信用分')
+        unlock_credit_achievements_by_name(
+            semester.start_date, semester.end_date-timedelta(days=1), '一学期没有扣除信用分')
         # 进行'当月没有扣除信用分'成就的触发
         if last_month_firstday < semester.end_date:
-            if (semester.end_date - last_month_firstday).days >= DAYS_LIMIT: # 上月第一天距离学期末的天数超过阈值
-                unlock_credit_achievements_by_name(last_month_firstday, last_month_lastday, '当月没有扣除信用分')
+            # 上月在学期内的天数超过阈值
+            if (semester.end_date - last_month_firstday).days >= DAYS_LIMIT: 
+                unlock_credit_achievements_by_name(
+                    last_month_firstday, last_month_lastday, '当月没有扣除信用分')
     # 简单起见，不妨每年7月1日进行'一学年没有扣除信用分'成就触发
     if today.month == 7:
-        unlock_credit_achievements_by_name(today-timedelta(days=365), today, '一学年没有扣除信用分')
+        unlock_credit_achievements_by_name(
+            today-timedelta(days=365), today, '一学年没有扣除信用分')
 
 
 def new_school_year_achievements():
