57a855e8c67648027553a0bee9abe13429d5f80b
panhongtao <59553164+panhongtao07@users.noreply.github.com>
2023-09-25 10:29:28 +0800

    添加模块文档

--- utils/models/query.py
+++ utils/models/query.py
@@ -1,3 +1,26 @@
+'''查询引用字段
+
+提供获取查询字段的函数，且同时支持字段描述符、字段实例以及字段名称。
+
+在以往的代码中，我们经常会看到这样的代码::
+
+    class AModel(Model):
+        fkey_name = models.ForeignKey(...)
+
+    class BModel(Model):
+        fkey2_name = models.ForeignKey(A_Model)
+
+当跨越关系查询时，我们需要写很长的代码，这样不仅不美观，而且不利于代码的追踪修改，例如::
+
+    instance = BModel.objects.filter(fkey2_name__fkey_name__field_name=...)
+    values = BModel.objects.values('fkey2_name__fkey_name__field_name')
+
+如需修改`fkey2_name`字段的名称，则无法追踪以上代码，因此修改起来非常困难，且容易出错。
+这是因为我们使用字符串来引用字段来引用字段。如果我们通过字段本身来引用，就可以避免这个问题。
+
+Warning:
+    本模块的函数在模型定义时无法使用，因为字段描述符在模型类创建后才会被创建，但你可以在任何方法中使用。
+'''
 from typing import cast, Any, TypeAlias
 
 from django.db.models import Field, Q
