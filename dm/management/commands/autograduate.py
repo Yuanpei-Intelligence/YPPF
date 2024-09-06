b7c25f5660af63b87afa7b48ae74954b1b5efc68
Morgen-Kornblume <144081714+Morgen-Kornblume@users.noreply.github.com>
2024-09-06 10:31:54 +0800

    添加了批量毕业指定年级学生的脚本 (#842)
    * 添加批量毕业指定年级学生的脚本
    
    * 添加了防止误伤教师的判断项
    
    * 所有 comment 已解决，并额外修改 accept_promote 为 False
    
    * Formatting
    
    ---------
    
    Co-authored-by: pkuGenuine <64714966+pkuGenuine@users.noreply.github.com>

---
+++ dm/management/commands/autograduate.py
@@ -0,0 +1,37 @@
+from django.core.management.base import BaseCommand, CommandParser, CommandError
+from django.db import transaction
+
+from app.models import (
+    NaturalPerson,
+    User
+)
+
+class Command(BaseCommand):
+    help = '通过学号检定批量调整指定年级的学生状态为“已毕业”，延迟毕业的学生需通过管理站点手动再次调整回来'
+
+    def add_arguments(self, parser: CommandParser):
+        parser.add_argument(
+            '--year',
+            type=int,
+            help='指定年级,以两位方式填写(例如20)'
+        )
+
+    def handle(self, *args, **options):
+        year = options['year']
+        
+        # 检查参数是否为空
+        if year is None:
+            raise CommandError('请指定年级参数')
+
+        with transaction.atomic():
+            # 选出指定年级的自然人(通过学号前两位判断,person_id为学号)
+            NaturalPerson.objects.filter(
+                person_id__username__startswith=str(year),
+                identity = NaturalPerson.Identity.STUDENT).update(
+                    status = NaturalPerson.GraduateStatus.GRADUATED,
+                    accept_promote = False)
+            User.objects.filter(
+                username__startswith=str(year),
+                utype = User.Type.STUDENT).update(active = False)
+            # 打印结果
+            self.stdout.write(self.style.SUCCESS('成功将%d级学生状态调整为“已毕业”' % year))
