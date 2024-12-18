b8d1ff60d15bc02851f795e5f1010a8b73f0678b
TerryLee27501 <TerryLee27501@gmail.com>
2024-12-18 12:30:53 +0800

    修复消息提示无法即时更新的bug

--- app/views.py
+++ app/views.py
@@ -1817,6 +1817,7 @@ def notifications(request: HttpRequest):
 
     done_list = notification2Display(done_notifications)
     undone_list = notification2Display(undone_notifications)
+    notes_list = done_list + undone_list
 
     # 新版侧边栏, 顶栏等的呈现，采用 bar_display, 必须放在render前最后一步
     bar_display = utils.get_sidebar_and_navbar(request.user,
