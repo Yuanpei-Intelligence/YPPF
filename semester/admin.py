940f820f1f6c2239328162e34195cb5fe440c182
pkugenuine <pkugenuine@gmail.com>
2025-01-16 09:15:25 +0000

    Remove Semester Model admin

--- semester/admin.py
+++ semester/admin.py
@@ -2,13 +2,3 @@ from django.contrib import admin
 
 from semester.models import Semester, SemesterType
 
-
-@admin.register(Semester)
-class SemesterAdmin(admin.ModelAdmin):
-    list_display = ['year', 'type', 'start_date', 'end_date']
-    list_filter = ['year', 'type']
-
-
-@admin.register(SemesterType)
-class SemesterTypeAdmin(admin.ModelAdmin):
-    pass
