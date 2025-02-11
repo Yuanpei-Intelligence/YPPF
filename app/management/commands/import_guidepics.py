d35270498c542dbbd420ada5423be9fc7fc2dac9
Deophius <NO1zhuce@163.com>
2025-02-11 13:19:05 +0000

    Add management command to import existing redirect.json

---
+++ app/management/commands/import_guidepics.py
@@ -0,0 +1,23 @@
+import json
+
+from django.core.files import File
+from django.core.management.base import BaseCommand
+from django.db import transaction
+
+from app.models import GuidePicture
+
+class Command(BaseCommand):
+    help = 'Import the home page guide pictures into the database from static/assets/img/guidepics/redirect.json'
+
+    def handle(self, *args, **kwargs):
+        guide_pic_dir = 'static/assets/img/guidepics'
+        with open(f'{guide_pic_dir}/redirect.json') as f:
+            a = json.load(f)
+        with transaction.atomic():
+            for (name, url) in a.items():
+                with open(f'{guide_pic_dir}/{name}', 'rb') as f:
+                    # Django file
+                    df = File(f)
+                    df.name = name
+                    GuidePicture.objects.create(redirect_url = url, image = df)
+                print(f'Uploaded {name}, redirecting to "{url}"')
