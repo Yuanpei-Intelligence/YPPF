4352d89b75efe97d117788290ad14107120f511a
HelloWorldZTR <2279429006@qq.com>
2025-07-16 20:42:22 +0800

    改为内存内排序，防止死锁

--- Appointment/views.py
+++ Appointment/views.py
@@ -977,6 +977,8 @@ def checkout_appoint(request: UserRequest):
                                         reverse("Appointment:account")))
                 except:
                     appoint.delete()
+                    conflict_appoints = sorted(conflict_appoints,
+                                               key=lambda x: (x.Astart, x.Afinish))
                     if conflict_appoints:
                         wrong(f"与预约时间为{conflict_appoints[0].Astart}"
                               + f"-{conflict_appoints[0].Afinish}的预约发生冲突",
